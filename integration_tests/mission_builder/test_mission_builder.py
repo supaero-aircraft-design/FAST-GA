856d7decad11028208db4ee3a8a547c8b4f123a2
Florent <florent.lutz2@isae-supaero.fr>
2021-11-08 10:43:03 +0100

    Mission Builder almost working
    Mission builder almost working, issue with the engine count name that is data:geometry:propulsion:count in FAST-OAD-GA and data:geometry:propulsion:engine:count in FAST-OAD, to be solved in next commit
    Other isue linked to a bug in FAST-OAD where no matter the engine setting in the mission file, the one actually used never changed.

--- integration_tests/mission_builder/test_mission_builder.py
+++ integration_tests/mission_builder/test_mission_builder.py
@@ -2,7 +2,11 @@ import os.path as pth
 
 from fastoad.io.configuration.configuration import FASTOADProblemConfigurator
 
+from fastga.models.geometry.geom_components.nacelle.compute_nacelle import ComputeNacelleGeometry
+
 DATA_FOLDER_PATH = pth.join(pth.dirname(__file__), "data")
+
+from tests.testing_utilities import run_system, get_indep_var_comp, list_inputs
 
 
 def test_mission():
@@ -16,3 +20,18 @@ def test_mission():
     problem.setup()
     problem.run_model()
     problem.write_outputs()
+
+
+def _test_engine_alone():
+    ivc = get_indep_var_comp(
+        list_inputs(
+            ComputeNacelleGeometry(propulsion_id="fastga.wrapper.propulsion.basicIC_engine")
+        ),
+        __file__,
+        pth.join(DATA_FOLDER_PATH, "source_input.xml"),
+    )
+
+    # Run problem and check obtained value(s) is/(are) correct
+    problem = run_system(
+        ComputeNacelleGeometry(propulsion_id="fastga.wrapper.propulsion.basicIC_engine"), ivc
+    )
