b8542fdebf5c10e20f51a10958bce167909d58f1
Florent <66668699+florentLutz@users.noreply.github.com>
2021-11-29 09:51:24 +0100

    Solving two issues (test)

--- src/fastga/models/aerodynamics/components/compute_propeller_aero.py
+++ src/fastga/models/aerodynamics/components/compute_propeller_aero.py
@@ -1,4 +1,4 @@
-"""Computation of propeller aero properties. """
+"""Computation of propeller aero properties."""
 #  This file is part of FAST : A framework for rapid Overall Aircraft Design
 #  Copyright (C) 2020  ONERA & ISAE-SUPAERO
 #  FAST is free software: you can redistribute it and/or modify
