Change in the computation of the flaps pitching moment coefficient and bug correction
Re-implemented the computation of the flaps pitching moment computation to better fit the method descibed in Roskam with x_ref at the wing quarter chord.
Also corrected a bug in the mission computation where the altitude was considered in feet for density computation but was actually in meter (issue #130)

src/fastga/models/performances/unitary_tests/test_cirrus_sr22.py
@@ -255,11 +255,11 @@ def test_compute_descent():
     # Run problem and check obtained value(s) is/(are) correct
     problem = run_system(group, ivc)
     fuel_mass = problem.get_val("data:mission:sizing:main_route:descent:fuel", units="kg")
-    assert fuel_mass == pytest.approx(0.61, abs=1e-2)
+    assert fuel_mass == pytest.approx(0.83, abs=1e-2)
     distance = (
         problem.get_val("data:mission:sizing:main_route:descent:distance", units="m") / 1000
     )  # conversion to km
-    assert distance == pytest.approx(80, abs=1)
+    assert distance == pytest.approx(87, abs=1)
     duration = problem.get_val("data:mission:sizing:main_route:descent:duration", units="min")
     assert duration == pytest.approx(27, abs=1)
 
@@ -300,7 +300,7 @@ def test_payload_range():
     payload_result = np.array([420, 420, 355, 310.697, 0])
     assert np.max(np.abs(payload_array - payload_result)) <= 1e-1
     range_array = problem.get_val("data:payload_range:range_array", units="NM")
-    range_result = np.array([0.0, 1169.73, 1632.40, 1955.12, 2210.96])
+    range_result = np.array([0.0, 1171.88, 1634.65, 1957.44, 2213.56])
     assert np.max(np.abs(range_array - range_result)) <= 1e-1
     specific_range_array = problem.get_val("data:payload_range:specific_range_array", units="NM/kg")
     specific_range_result = np.array([0.0, 6.24, 6.47, 6.58, 7.45])

src/fastga/utils/postprocessing/analysis_and_plots.py
@@ -29,6 +29,8 @@ from openmdao.utils.units import convert_units
 from fastoad.io import VariableIO
 from fastoad.openmdao.variables import VariableList
 
+from fastga.models.aerodynamics.components.compute_equilibrated_polar import FIRST_INVALID_COEFF
+
 COLS = plotly.colors.DEFAULT_PLOTLY_COLORS
 
 
@@ -1194,26 +1196,42 @@ def aircraft_polar(
 
     if equilibrated:
         cl_array_cruise = list(variables["data:aerodynamics:aircraft:cruise:equilibrated:CL"].value)
-        cl_array_cruise = [e for i, e in enumerate(cl_array_cruise) if e != 0]
+        cl_array_cruise = [
+            e for i, e in enumerate(cl_array_cruise) if e != 0 and e < FIRST_INVALID_COEFF
+        ]
         cd_array_cruise = list(variables["data:aerodynamics:aircraft:cruise:equilibrated:CD"].value)
-        cd_array_cruise = [e for i, e in enumerate(cd_array_cruise) if e != 0]
+        cd_array_cruise = [
+            e for i, e in enumerate(cd_array_cruise) if e != 0 and e < FIRST_INVALID_COEFF
+        ]
         cl_array_low_speed = list(
             variables["data:aerodynamics:aircraft:low_speed:equilibrated:CL"].value
         )
-        cl_array_low_speed = [e for i, e in enumerate(cl_array_low_speed) if e != 0]
+        cl_array_low_speed = [
+            e for i, e in enumerate(cl_array_low_speed) if e != 0 and e < FIRST_INVALID_COEFF
+        ]
         cd_array_low_speed = list(
             variables["data:aerodynamics:aircraft:low_speed:equilibrated:CD"].value
         )
-        cd_array_low_speed = [e for i, e in enumerate(cd_array_low_speed) if e != 0]
+        cd_array_low_speed = [
+            e for i, e in enumerate(cd_array_low_speed) if e != 0 and e < FIRST_INVALID_COEFF
+        ]
     else:
         cl_array_cruise = list(variables["data:aerodynamics:aircraft:cruise:CL"].value)
-        cl_array_cruise = [e for i, e in enumerate(cl_array_cruise) if e != 0]
+        cl_array_cruise = [
+            e for i, e in enumerate(cl_array_cruise) if e != 0 and e < FIRST_INVALID_COEFF
+        ]
         cd_array_cruise = list(variables["data:aerodynamics:aircraft:cruise:CD"].value)
-        cd_array_cruise = [e for i, e in enumerate(cd_array_cruise) if e != 0]
+        cd_array_cruise = [
+            e for i, e in enumerate(cd_array_cruise) if e != 0 and e < FIRST_INVALID_COEFF
+        ]
         cl_array_low_speed = list(variables["data:aerodynamics:aircraft:low_speed:CL"].value)
-        cl_array_low_speed = [e for i, e in enumerate(cl_array_low_speed) if e != 0]
+        cl_array_low_speed = [
+            e for i, e in enumerate(cl_array_low_speed) if e != 0 and e < FIRST_INVALID_COEFF
+        ]
         cd_array_low_speed = list(variables["data:aerodynamics:aircraft:low_speed:CD"].value)
-        cd_array_low_speed = [e for i, e in enumerate(cd_array_low_speed) if e != 0]
+        cd_array_low_speed = [
+            e for i, e in enumerate(cd_array_low_speed) if e != 0 and e < FIRST_INVALID_COEFF
+        ]
 
     # Computation of the highest CL/CD ratio which gives the L/D max.
     l_d_max_cruise = max(np.asarray(cl_array_cruise) / np.asarray(cd_array_cruise))