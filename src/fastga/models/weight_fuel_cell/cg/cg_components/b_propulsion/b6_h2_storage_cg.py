Corrected CG calculation with hybrid-electric components. Added a submodule for empty cg calculation
There is a problem in horizontal tail surface area calculation.

src/fastga/models/weight_fuel_cell/cg/cg_components/b_propulsion/b6_h2_storage_cg.py
@@ -32,7 +32,7 @@ class ComputeH2StorageCG(ExplicitComponent):
         self.add_input("data:geometry:cabin:length", val=np.nan, units="m")
 
         self.add_output("data:weight:propulsion:tank:CG:x", units="m")
-        # self.add_output("data:weight:hybrid_powertrain:h2_storage:CG:x", units="m")
+        self.add_output("data:weight:hybrid_powertrain:h2_storage:CG:x", units="m")
 
         self.declare_partials("*", "*", method="fd")
 
@@ -43,5 +43,5 @@ class ComputeH2StorageCG(ExplicitComponent):
         # H2 tanks assumed to be placed just behind the pilot seat (stacked in height if more than 1)
         b6 = fus_front_length + 0.9 * cabin_length  # 90% of the cabin length
 
-        outputs["data:weight:propulsion:tank:CG:x"] = b6
-        # outputs["data:weight:hybrid_powertrain:h2_storage:CG:x"] = b6
+        outputs["data:weight:propulsion:tank:CG:x"] = b6 # This is for loadcases with fuel weight
+        outputs["data:weight:hybrid_powertrain:h2_storage:CG:x"] = b6 # this is for empty CG

src/fastga/models/weight_fuel_cell/cg/cg_components/b_propulsion/b_cg.py
@@ -42,11 +42,13 @@ class ComputeFuelPropulsionCG(om.ExplicitComponent):
         self.add_input("data:weight:propulsion:fuel_lines:CG:x", units="m", val=np.nan)
         self.add_input("data:weight:hybrid_powertrain:fuel_cell:CG:x", units="m", val=np.nan)
         self.add_input("data:weight:hybrid_powertrain:battery:CG:x", units="m", val=np.nan)
+        self.add_input("data:weight:hybrid_powertrain:h2_storage:CG:x", units='m', val=np.nan)
         self.add_input("data:weight:hybrid_powertrain:engine:mass", units="kg", val=np.nan)
         self.add_input("data:weight:propulsion:fuel_lines:mass", units="kg", val=np.nan)
         self.add_input("data:weight:hybrid_powertrain:fuel_cell:mass", units="kg", val=np.nan)
         self.add_input("data:weight:hybrid_powertrain:battery:mass", units="kg", val=np.nan)
-        # self.add_input("data:weight:propulsion:mass", units="kg", val=np.nan)
+        self.add_input("data:weight:hybrid_powertrain:h2_storage:mass", units="kg", val=np.nan)
+
 
         self.add_output("data:weight:propulsion:CG:x", units="m")
 
@@ -55,15 +57,17 @@ class ComputeFuelPropulsionCG(om.ExplicitComponent):
         fuel_lines_cg = inputs["data:weight:propulsion:fuel_lines:CG:x"]
         fuel_cell_cg = inputs["data:weight:hybrid_powertrain:fuel_cell:CG:x"]
         battery_cg = inputs["data:weight:hybrid_powertrain:battery:CG:x"]
+        h2_tanks_cg = inputs["data:weight:hybrid_powertrain:h2_storage:CG:x"]
 
         engine_mass = inputs["data:weight:hybrid_powertrain:engine:mass"]
         fuel_lines_mass = inputs["data:weight:propulsion:fuel_lines:mass"]
         fuel_cells_mass = inputs["data:weight:hybrid_powertrain:fuel_cell:mass"]
         battery_mass = inputs["data:weight:hybrid_powertrain:battery:mass"]
+        h2_tanks_mass = inputs["data:weight:hybrid_powertrain:h2_storage:mass"]
 
         cg_propulsion = (engine_cg * engine_mass + fuel_lines_cg * fuel_lines_mass +
-                         fuel_cell_cg*fuel_cells_mass + battery_cg*battery_mass) / (
-            engine_mass + fuel_lines_mass + fuel_cells_mass + battery_mass
+                         fuel_cell_cg*fuel_cells_mass + battery_cg*battery_mass + h2_tanks_cg*h2_tanks_mass) / (
+            engine_mass + fuel_lines_mass + fuel_cells_mass + battery_mass + h2_tanks_mass
         )
 
         outputs["data:weight:propulsion:CG:x"] = cg_propulsion