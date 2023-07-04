8a0e5b1770debc6d0756ed7a5f8e71777fbda10f
Muhamad Abdul Aziz <79506671+Muhabdaziz@users.noreply.github.com>
2023-07-04 10:35:15 +0200

    Update aero_center.py

--- src/fastga/models/aerodynamics/aero_center.py
+++ src/fastga/models/aerodynamics/aero_center.py
@@ -15,14 +15,32 @@
 
 import numpy as np
 from openmdao.core.explicitcomponent import ExplicitComponent
+from openmdao.core.group import Group
 from stdatm import Atmosphere
 
 
-class ComputeAeroCenter(ExplicitComponent):
+class ComputeAeroCenter(Group):
     # TODO: Document equations. Cite sources
     """Aerodynamic center estimation."""
 
     def setup(self):
+
+        self.add_subsystem(
+            "comp_aero_center_stick_fixed", ComputeAeroCenterStickFixed(), promotes=["*"],
+        )
+        self.add_subsystem(
+            "comp_aero_center_free_elevator_factor", ComputeAeroCenterFreeElevatorFactor(), promotes=["*"],
+        )
+        self.add_subsystem(
+            "comp_aero_center_stick_free", ComputeAeroCenterStickFree(), promotes=["*"],
+        )
+
+
+class ComputeAeroCenterStickFixed(ExplicitComponent):
+    """Aerodynamic center estimation - Stick Fixed"""
+
+    def setup(self):
+
         self.add_input("data:geometry:wing:MAC:length", val=np.nan, units="m")
         self.add_input(
             "data:geometry:horizontal_tail:MAC:at25percent:x:from_wingMAC25", val=np.nan, units="m"
@@ -31,9 +49,42 @@ class ComputeAeroCenter(ExplicitComponent):
         self.add_input(
             "data:aerodynamics:horizontal_tail:cruise:CL_alpha", val=np.nan, units="rad**-1"
         )
+        self.add_input("data:aerodynamics:fuselage:cm_alpha", val=np.nan, units="rad**-1")
+        self.add_input("data:aerodynamics:horizontal_tail:efficiency", val=np.nan)
+
+        self.add_output("data:aerodynamics:cruise:neutral_point:stick_fixed:x")
+
+        self.declare_partials("*", "*", method="fd")
+
+    def compute(self, inputs, outputs, discrete_inputs=None, discrete_outputs=None):
+
+        l0_wing = inputs["data:geometry:wing:MAC:length"]
+        lp_ht = inputs["data:geometry:horizontal_tail:MAC:at25percent:x:from_wingMAC25"]
+        cl_alpha_wing = inputs["data:aerodynamics:wing:cruise:CL_alpha"]
+        cl_alpha_ht = inputs["data:aerodynamics:horizontal_tail:cruise:CL_alpha"]
+        cm_alpha_fus = inputs["data:aerodynamics:fuselage:cm_alpha"]
+        tail_efficiency = inputs["data:aerodynamics:horizontal_tail:efficiency"]
+
+        # TODO: make variable name in computation sequence more english
+        # FIXME: introduce cm_alpha_wing to the equation (non-symmetrical profile)
+        x_ca_plane = (tail_efficiency * cl_alpha_ht * lp_ht - cm_alpha_fus * l0_wing) / (
+            cl_alpha_wing + tail_efficiency * cl_alpha_ht
+        )
+        x_aero_center = x_ca_plane / l0_wing + 0.25
+
+        outputs["data:aerodynamics:cruise:neutral_point:stick_fixed:x"] = x_aero_center
+
+
+class ComputeAeroCenterFreeElevatorFactor(ExplicitComponent):
+    """Aerodynamic center estimation - Free Elevator Factor"""
+
+    def setup(self):
+
+        self.add_input(
+            "data:aerodynamics:horizontal_tail:cruise:CL_alpha", val=np.nan, units="rad**-1"
+        )
         self.add_input("data:aerodynamics:horizontal_tail:cruise:downwash_gradient", val=np.nan)
         self.add_input("data:aerodynamics:elevator:low_speed:CL_delta", val=np.nan, units="rad**-1")
-        self.add_input("data:aerodynamics:fuselage:cm_alpha", val=np.nan, units="rad**-1")
         self.add_input(
             "data:aerodynamics:horizontal_tail:cruise:hinge_moment:CH_alpha",
             val=np.nan,
@@ -44,39 +95,22 @@ class ComputeAeroCenter(ExplicitComponent):
             val=np.nan,
             units="rad**-1",
         )
-        self.add_input("data:aerodynamics:horizontal_tail:efficiency", val=np.nan)
         self.add_input("data:TLAR:v_cruise", val=np.nan, units="m/s")
         self.add_input("data:mission:sizing:main_route:cruise:altitude", val=np.nan, units="ft")
 
-        self.add_output("data:aerodynamics:cruise:neutral_point:stick_fixed:x")
-        self.add_output("data:aerodynamics:cruise:neutral_point:stick_free:x")
         self.add_output("data:aerodynamics:cruise:neutral_point:free_elevator_factor")
 
-        self.declare_partials("*", "*", method="fd")
+        self.declare_partials("*", "*", method="fd")        
 
     def compute(self, inputs, outputs, discrete_inputs=None, discrete_outputs=None):
 
-        l0_wing = inputs["data:geometry:wing:MAC:length"]
-        lp_ht = inputs["data:geometry:horizontal_tail:MAC:at25percent:x:from_wingMAC25"]
-        cl_alpha_wing = inputs["data:aerodynamics:wing:cruise:CL_alpha"]
         cl_alpha_ht = inputs["data:aerodynamics:horizontal_tail:cruise:CL_alpha"]
         downwash_gradient = inputs["data:aerodynamics:horizontal_tail:cruise:downwash_gradient"]
         cl_delta_ht = inputs["data:aerodynamics:elevator:low_speed:CL_delta"]
         ch_alpha_3d = inputs["data:aerodynamics:horizontal_tail:cruise:hinge_moment:CH_alpha"]
         ch_delta_3d = inputs["data:aerodynamics:horizontal_tail:cruise:hinge_moment:CH_delta"]
-        cm_alpha_fus = inputs["data:aerodynamics:fuselage:cm_alpha"]
-        tail_efficiency = inputs["data:aerodynamics:horizontal_tail:efficiency"]
         v_cruise = inputs["data:TLAR:v_cruise"]
         alt_cruise = inputs["data:mission:sizing:main_route:cruise:altitude"]
-
-        # TODO: make variable name in computation sequence more english
-        # FIXME: introduce cm_alpha_wing to the equation (non-symmetrical profile)
-        x_ca_plane = (tail_efficiency * cl_alpha_ht * lp_ht - cm_alpha_fus * l0_wing) / (
-            cl_alpha_wing + tail_efficiency * cl_alpha_ht
-        )
-        x_aero_center = x_ca_plane / l0_wing + 0.25
-
-        outputs["data:aerodynamics:cruise:neutral_point:stick_fixed:x"] = x_aero_center
 
         sos = Atmosphere(alt_cruise).speed_of_sound
         mach = v_cruise / sos
@@ -97,6 +131,38 @@ class ComputeAeroCenter(ExplicitComponent):
         outputs[
             "data:aerodynamics:cruise:neutral_point:free_elevator_factor"
         ] = free_elevator_factor
+
+
+class ComputeAeroCenterStickFree(ExplicitComponent):
+    """Aerodynamic center estimation - Stick Free"""
+
+    def setup(self):
+
+        self.add_input("data:geometry:wing:MAC:length", val=np.nan, units="m")
+        self.add_input(
+            "data:geometry:horizontal_tail:MAC:at25percent:x:from_wingMAC25", val=np.nan, units="m"
+        )
+        self.add_input("data:aerodynamics:wing:cruise:CL_alpha", val=np.nan, units="rad**-1")
+        self.add_input(
+            "data:aerodynamics:horizontal_tail:cruise:CL_alpha", val=np.nan, units="rad**-1"
+        )
+        self.add_input("data:aerodynamics:fuselage:cm_alpha", val=np.nan, units="rad**-1")
+        self.add_input("data:aerodynamics:horizontal_tail:efficiency", val=np.nan)
+        self.add_input("data:aerodynamics:cruise:neutral_point:free_elevator_factor", val=np.nan)
+
+        self.add_output("data:aerodynamics:cruise:neutral_point:stick_free:x")
+
+        self.declare_partials("*", "*", method="fd")
+
+    def compute(self, inputs, outputs, discrete_inputs=None, discrete_outputs=None):
+
+        l0_wing = inputs["data:geometry:wing:MAC:length"]
+        lp_ht = inputs["data:geometry:horizontal_tail:MAC:at25percent:x:from_wingMAC25"]
+        cl_alpha_wing = inputs["data:aerodynamics:wing:cruise:CL_alpha"]
+        cl_alpha_ht = inputs["data:aerodynamics:horizontal_tail:cruise:CL_alpha"]
+        cm_alpha_fus = inputs["data:aerodynamics:fuselage:cm_alpha"]
+        tail_efficiency = inputs["data:aerodynamics:horizontal_tail:efficiency"]
+        free_elevator_factor = inputs["data:aerodynamics:cruise:neutral_point:free_elevator_factor"]
 
         x_ca_plane_free = (
             tail_efficiency * free_elevator_factor * cl_alpha_ht * lp_ht - cm_alpha_fus * l0_wing
