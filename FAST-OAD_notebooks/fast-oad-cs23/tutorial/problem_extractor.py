7d394df707fe6bd09544dc113df800862079084b
Biel Galiot Pérez <39485660+bielgaliot@users.noreply.github.com>
2023-07-04 10:40:28 +0200

    Created problem extractor, that taims at obtaining the problem structure from the config file, using some of the functions used for creating the n2 matrix

---
+++ FAST-OAD_notebooks/fast-oad-cs23/tutorial/problem_extractor.py
@@ -0,0 +1,39 @@
+from fastoad.io.configuration import FASTOADProblemConfigurator
+from openmdao.visualization.n2_viewer.n2_viewer import _get_viewer_data
+from openmdao.utils.om_warnings import issue_warning
+import os.path as pth
+import os
+
+# Define relative path
+DATA_FOLDER_PATH = "data"
+WORK_FOLDER_PATH = "workdir"
+CONFIGURATION_FILE = pth.join(WORK_FOLDER_PATH, "oad_process_test.yml")
+
+# Used for test purposes only
+_PROBLEM_CONFIGURATOR = None
+
+#Setup of the problem
+conf = FASTOADProblemConfigurator(CONFIGURATION_FILE)
+conf._set_configuration_modifier(_PROBLEM_CONFIGURATOR)
+problem = conf.get_problem()
+problem.setup()
+problem.final_setup()
+
+#convert problem to dictionary
+case_id=None
+try:
+    model_data = _get_viewer_data(problem, case_id=case_id)
+    err_msg = ''
+except TypeError as err:
+    model_data = {}
+    err_msg = str(err)
+    issue_warning(err_msg)
+
+print(type(model_data))
+#print(model_data.keys())
+#dict_keys(['tree', 'md5_hash', 'sys_pathnames_list', 'connections_list', 'abs2prom', 'driver', 'design_vars', 'responses', 'declare_partials_list'])
+print(str(model_data['connections_list']))
+
+with open('output.txt', 'a') as f:
+    f.write(str(model_data['connections_list']))
+#print(model_data.values())
--- FAST-OAD_notebooks/fast-oad-cs23/tutorial/xdsm_generator.py
+++ FAST-OAD_notebooks/fast-oad-cs23/tutorial/xdsm_generator.py
@@ -1,6 +1,7 @@
 #import warnings
-import timeit
-starting = timeit.timeit()
+import time
+
+starting = time.time()
 from Count_Under_XDSM_Diag import *
 
 #warnings.filterwarnings(action="ignore")
@@ -8,7 +9,8 @@ from Count_Under_XDSM_Diag import *
 import os.path as pth
 import os
 #import openmdao.api as om
-from fastoad import api as api_cs25
+#from fastoad import api as api_cs25
+from fastoad.api import write_xdsm as write_xdsm
 #from fastga.command import api as api_cs23
 #import logging
 #from fastoad.gui import VariableViewer
@@ -28,7 +30,6 @@ CONFIGURATION_FILE = pth.join(WORK_FOLDER_PATH, "oad_process_test.yml")
 # For having log messages on screen
 #logging.basicConfig(level=logging.INFO, format="%(levelname)-8s: %(message)s")
 
-
 """api_cs25.generate_configuration_file(
     CONFIGURATION_FILE,
     overwrite=True,
@@ -36,14 +37,13 @@ CONFIGURATION_FILE = pth.join(WORK_FOLDER_PATH, "oad_process_test.yml")
     sample_file_name="fastga.yml",
 )"""
 
-
 XDSM_FILE = pth.join(WORK_FOLDER_PATH, "xdsm.html")
-api_cs25.write_xdsm(CONFIGURATION_FILE, XDSM_FILE, overwrite=True, depth=-1)
+write_xdsm(CONFIGURATION_FILE, XDSM_FILE, overwrite=True, depth = -1) #depth = -1 to reach as far into the sub-modules as we can go, stopping before the variables themselves
 
 print('There are', Count_Under_XDSM_Diag(XDSM_FILE), 'elements under the diagonal')
 
 
-ending = timeit.timeit()
+ending = time.time()
 
 print('Time taken in this iteration:', ending - starting)
 
