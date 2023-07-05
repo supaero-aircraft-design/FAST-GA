cc2ad7bd23dfc0ac4254dcbe2045af120ced9a74
Muhamad Abdul Aziz <79506671+Muhabdaziz@users.noreply.github.com>
2023-07-05 10:18:01 +0200

    Update compute_wing_x_absolute.py

--- src/fastga/models/geometry/geom_components/wing/components/compute_wing_x_absolute.py
+++ src/fastga/models/geometry/geom_components/wing/components/compute_wing_x_absolute.py
@@ -15,6 +15,7 @@
 import numpy as np
 
 from openmdao.core.explicitcomponent import ExplicitComponent
+from openmdao.core.group import Group
 
 import fastoad.api as oad
 
@@ -22,7 +23,7 @@ from ..constants import SUBMODEL_WING_X_ABSOLUTE
 
 
 @oad.RegisterSubmodel(SUBMODEL_WING_X_ABSOLUTE, "fastga.submodel.geometry.wing.x_absolute.legacy")
-class ComputeWingXAbsolute(ExplicitComponent):
+class ComputeWingXAbsolute(Group):
     """
     Wing absolute Xs estimation, distance from the nose to the leading edge at different
     section.
@@ -30,56 +31,76 @@ class ComputeWingXAbsolute(ExplicitComponent):
 
     def setup(self):
 
-        self.add_input("data:geometry:wing:tip:leading_edge:x:local", units="m", val=np.nan)
+        self.add_subsystem("comp_wing_x_abs_from_mac", ComputeXAbsoluteMac(), promotes=["*"])
+        self.add_subsystem("comp_wing_x_abs_from_tip", ComputeXAbsoluteTip(), promotes=["*"])
+
+
+class ComputeXAbsoluteMac(ExplicitComponent):
+    """
+    Wing absolute Xs estimation, distance from the nose to the MAC leading edge
+    """
+
+    def setup(self):
+
         self.add_input("data:geometry:wing:MAC:length", units="m", val=np.nan)
         self.add_input("data:geometry:wing:MAC:at25percent:x", units="m", val=np.nan)
-        self.add_input("data:geometry:wing:MAC:leading_edge:x:local", units="m", val=np.nan)
 
-        self.add_output("data:geometry:wing:tip:leading_edge:x:absolute", units="m")
         self.add_output("data:geometry:wing:MAC:leading_edge:x:absolute", units="m")
 
-        self.declare_partials(
-            of="data:geometry:wing:MAC:leading_edge:x:absolute",
-            wrt=[
-                "data:geometry:wing:MAC:at25percent:x",
-                "data:geometry:wing:MAC:length",
-            ],
-            method="exact",
-        )
-        self.declare_partials(
-            of="data:geometry:wing:tip:leading_edge:x:absolute",
-            wrt="*",
-            method="exact",
-        )
+        self.declare_partials("*", "*", method="exact")
 
     def compute(self, inputs, outputs, discrete_inputs=None, discrete_outputs=None):
 
         fa_length = inputs["data:geometry:wing:MAC:at25percent:x"]
         l0_wing = inputs["data:geometry:wing:MAC:length"]
-        x_local_mac = inputs["data:geometry:wing:MAC:leading_edge:x:local"]
-        x_local_tip = inputs["data:geometry:wing:tip:leading_edge:x:local"]
 
         x_abs_mac = fa_length - 0.25 * l0_wing
-        x_abs_tip = x_abs_mac - x_local_mac + x_local_tip
 
         outputs["data:geometry:wing:MAC:leading_edge:x:absolute"] = x_abs_mac
+
+    def compute_partials(self, inputs, partials, discrete_inputs=None):
+
+        partials[
+            "data:geometry:wing:MAC:leading_edge:x:absolute",
+            "data:geometry:wing:MAC:at25percent:x",
+        ] = 1.0
+        partials[
+            "data:geometry:wing:MAC:leading_edge:x:absolute",
+            "data:geometry:wing:MAC:length",
+        ] = -0.25
+
+
+class ComputeXAbsoluteTip(ExplicitComponent):
+    """
+    Wing absolute Xs estimation, distance from the nose to the wing tip leading edge
+    """
+
+    def setup(self):
+
+        self.add_input("data:geometry:wing:tip:leading_edge:x:local", units="m", val=np.nan)
+        self.add_input("data:geometry:wing:MAC:leading_edge:x:local", units="m", val=np.nan)
+        self.add_input("data:geometry:wing:MAC:leading_edge:x:absolute", units="m", val=np.nan)
+
+        self.add_output("data:geometry:wing:tip:leading_edge:x:absolute", units="m")
+
+        self.declare_partials("*", "*", method="exact")
+
+    def compute(self, inputs, outputs, discrete_inputs=None, discrete_outputs=None):
+
+        x_local_mac = inputs["data:geometry:wing:MAC:leading_edge:x:local"]
+        x_local_tip = inputs["data:geometry:wing:tip:leading_edge:x:local"]
+        x_abs_mac = inputs["data:geometry:wing:MAC:leading_edge:x:absolute"]
+
+        x_abs_tip = x_abs_mac - x_local_mac + x_local_tip
+
         outputs["data:geometry:wing:tip:leading_edge:x:absolute"] = x_abs_tip
 
     def compute_partials(self, inputs, partials, discrete_inputs=None):
 
         partials[
-            "data:geometry:wing:MAC:leading_edge:x:absolute", "data:geometry:wing:MAC:at25percent:x"
+            "data:geometry:wing:tip:leading_edge:x:absolute",
+            "data:geometry:wing:MAC:leading_edge:x:absolute",
         ] = 1.0
-        partials[
-            "data:geometry:wing:MAC:leading_edge:x:absolute", "data:geometry:wing:MAC:length"
-        ] = -0.25
-
-        partials[
-            "data:geometry:wing:tip:leading_edge:x:absolute", "data:geometry:wing:MAC:at25percent:x"
-        ] = 1.0
-        partials[
-            "data:geometry:wing:tip:leading_edge:x:absolute", "data:geometry:wing:MAC:length"
-        ] = -0.25
         partials[
             "data:geometry:wing:tip:leading_edge:x:absolute",
             "data:geometry:wing:MAC:leading_edge:x:local",
