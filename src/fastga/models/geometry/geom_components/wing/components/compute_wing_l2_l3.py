129ff9886df651633a61930c36c76b5ac4713f82
Muhamad Abdul Aziz <79506671+Muhabdaziz@users.noreply.github.com>
2023-07-05 08:27:33 +0200

    Update compute_wing_l2_l3.py

--- src/fastga/models/geometry/geom_components/wing/components/compute_wing_l2_l3.py
+++ src/fastga/models/geometry/geom_components/wing/components/compute_wing_l2_l3.py
@@ -16,6 +16,7 @@
 import numpy as np
 
 from openmdao.core.explicitcomponent import ExplicitComponent
+from openmdao.core.group import Group
 
 import fastoad.api as oad
 
@@ -23,9 +24,18 @@ from ..constants import SUBMODEL_WING_L2_L3
 
 
 @oad.RegisterSubmodel(SUBMODEL_WING_L2_L3, "fastga.submodel.geometry.wing.l2_l3.legacy")
-class ComputeWingL2AndL3(ExplicitComponent):
+class ComputeWingL2AndL3(Group):
     # TODO: Document equations. Cite sources
     """Wing chords (l2 and l3) estimation."""
+
+    def setup(self):
+
+        self.add_subsystem("comp_wing_l2", ComputeWingL2(), promotes=["*"])
+        self.add_subsystem("comp_wing_l3", ComputeWingL3(), promotes=["*"])
+
+
+class ComputeWingL2(ExplicitComponent):
+    """Estimate l2 wing chord."""
 
     def setup(self):
 
@@ -35,12 +45,11 @@ class ComputeWingL2AndL3(ExplicitComponent):
         self.add_input("data:geometry:wing:taper_ratio", val=np.nan)
 
         self.add_output("data:geometry:wing:root:chord", units="m")
-        self.add_output("data:geometry:wing:kink:chord", units="m")
 
         self.declare_partials("*", "*", method="fd")
 
     def compute(self, inputs, outputs, discrete_inputs=None, discrete_outputs=None):
-
+        
         wing_area = inputs["data:geometry:wing:area"]
         y2_wing = inputs["data:geometry:wing:root:y"]
         y4_wing = inputs["data:geometry:wing:tip:y"]
@@ -48,7 +57,26 @@ class ComputeWingL2AndL3(ExplicitComponent):
 
         l2_wing = wing_area / (2.0 * y2_wing + (y4_wing - y2_wing) * (1.0 + taper_ratio))
 
+        outputs["data:geometry:wing:root:chord"] = l2_wing
+
+
+class ComputeWingL3(ExplicitComponent):
+    """Estimate l3 wing chord."""
+
+    def setup(self):
+
+        self.add_input("data:geometry:wing:root:chord", units="m")
+
+        self.add_output("data:geometry:wing:kink:chord", units="m")
+
+        self.declare_partials(
+            "data:geometry:wing:kink:chord", "data:geometry:wing:root:chord", val=1.0
+        )
+
+    def compute(self, inputs, outputs, discrete_inputs=None, discrete_outputs=None):
+        
+        l2_wing = inputs["data:geometry:wing:root:chord"]
+
         l3_wing = l2_wing
 
-        outputs["data:geometry:wing:root:chord"] = l2_wing
         outputs["data:geometry:wing:kink:chord"] = l3_wing
