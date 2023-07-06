39e07f16e4228fd5b929a7e9582306496b87b810
Biel Galiot Pérez <39485660+bielgaliot@users.noreply.github.com>
2023-07-06 14:40:33 +0200

    Double swap working in configuration_optimizer.py, Still needs a lot of work regarding performance

--- FAST-OAD_notebooks/fast-oad-cs23/tutorial/configuration_optimizer.py
+++ FAST-OAD_notebooks/fast-oad-cs23/tutorial/configuration_optimizer.py
@@ -33,22 +33,13 @@ def double_swap_algorithm(problem_dictionary, config_dictionary, CONFIGURATION_F
             keys_list[i], keys_list[-1] = keys_list[-1], keys_list[i]  
             print('Trying order: ', keys_list)
             #Generate new config file with proposed order
-            #new_ordered_config = {key: config_dictionary[key] for key in keys if key in config_dictionary}
             with open(CONFIGURATION_FILE, 'r') as file:
-                existing_data = yaml.safe_load(file)
-
-
-            #for key in keys_list:
-            #    existing_data['model']['aircraft_sizing'][key] = config_dictionary[key]
-
-            #existing_data['model']['aircraft_sizing'].update(keys) 
-            #   
+                existing_data = yaml.safe_load(file) 
             existing_data['model']['aircraft_sizing'] = {key: config_dictionary[key] for key in keys_list if key in config_dictionary}
-
             with open(CONFIGURATION_FILE, 'w') as file:
                 yaml.dump(existing_data, file, default_flow_style=False, sort_keys=False)
 
-            
+            #convert config file to problem and then to dictionary, as input for feedback_extractor
             conf = FASTOADProblemConfigurator(CONFIGURATION_FILE)
             problem = conf.get_problem()
             problem.setup()
@@ -85,6 +76,79 @@ def double_swap_algorithm(problem_dictionary, config_dictionary, CONFIGURATION_F
     return keys_list
 
 
+def single_swap_algorithm(problem_dictionary, config_dictionary, CONFIGURATION_FILE):
+
+    keys = config_dictionary.keys()
+    best_score = feedback_extractor(problem_dictionary)  # Initial score of the dictionary
+    keys_list = list(keys)
+    best_order = keys_list.copy()  # Initial order of keys
+
+    print('Starting order: ', keys_list)
+    counter = 0
+    while True:
+        counter =+ 1 
+        print(counter)
+        improvement = False
+        # Find a better position for the last node
+        for i in range(len(keys_list) - 1):
+
+            # Swap last node with the ith node (doube swap)
+            keys_list[i], keys_list[-1] = keys_list[-1], keys_list[i]  
+            print('Trying order: ', keys_list)
+            #Generate new config file with proposed order
+            with open(CONFIGURATION_FILE, 'r') as file:
+                existing_data = yaml.safe_load(file) 
+            existing_data['model']['aircraft_sizing'] = {key: config_dictionary[key] for key in keys_list if key in config_dictionary}
+            with open(CONFIGURATION_FILE, 'w') as file:
+                yaml.dump(existing_data, file, default_flow_style=False, sort_keys=False)
+
+            #convert config file to problem and then to dictionary, as input for feedback_extractor
+            conf = FASTOADProblemConfigurator(CONFIGURATION_FILE)
+            problem = conf.get_problem()
+            problem.setup()
+            problem.final_setup()
+            case_id=None
+            model_data = _get_viewer_data(problem, case_id=case_id)
+
+            #evaluate the score of the proposed order
+            score = feedback_extractor(model_data)
+            
+            if score < best_score:
+                best_score = score
+                best_order = list(keys).copy()
+                improvement = True
+                print('Swap Kept')
+                print('Current order: ', best_order)
+
+            else:
+                keys_list[i], keys_list[-1] = keys_list[-1], keys_list[i]  # Revert the swap
+                print('Swap reverted')
+                print('Current order: ', keys_list)
+
+        if not improvement:
+            break
+        
+    keys_list = best_order.copy()  # Start from the best order found so far
+    best_config = existing_data
+    best_config['model']['aircraft_sizing'] = {key: config_dictionary[key] for key in keys_list if key in config_dictionary}
+    #Write best configuration
+    with open(CONFIGURATION_FILE, 'w') as file:
+        yaml.dump(best_config, file, default_flow_style=False, sort_keys=False)
+    print('Best order has been written in configuration file')
+    return keys_list
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
@@ -93,7 +157,6 @@ WORK_FOLDER_PATH = "workdir"
 
 # Define file
 CONFIGURATION_FILE = pth.join(WORK_FOLDER_PATH, "oad_process_test.yml")
-
 
 #Define optimizatin level: (assumes presence of aircraft_sizing, with modules defined)
 # LVL 1: CONFIG FILE MODULES - shuffles the order of
@@ -113,6 +176,7 @@ CONFIGURATION_FILE = pth.join(WORK_FOLDER_PATH, "oad_process_test.yml")
 start = time.time()
 
 optimization_level = 1
+swap = 'DOUBLE'
 
 if optimization_level == 1:
 
@@ -131,8 +195,6 @@ if optimization_level == 1:
         model_data = {}
         err_msg = str(err)
         issue_warning(err_msg)
-
-
  
     with open(CONFIGURATION_FILE, 'r') as file:
 
@@ -150,7 +212,14 @@ if optimization_level == 1:
         aircraft_sizing_data = yaml_data['model']['aircraft_sizing']
 
         print('Starting order before first swap: ', aircraft_sizing_data.keys())
-        dummy_var = double_swap_algorithm(model_data, aircraft_sizing_data, CONFIGURATION_FILE)
+
+        if swap == 'DOUBLE':
+            dummy_var = double_swap_algorithm(model_data, aircraft_sizing_data, CONFIGURATION_FILE)
+        elif swap == 'SINGLE':
+            dummy_var = single_swap_algorithm(model_data, aircraft_sizing_data, CONFIGURATION_FILE)
+        elif swap == 'HYBRID':
+            e = 3
+        else: print('\Swap type not valid')
 
         print(dummy_var)
 
