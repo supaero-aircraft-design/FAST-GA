b9e926e1cd0a47c9e48e527ae4ae209a2f7a446d
Muhamad Abdul Aziz <79506671+Muhabdaziz@users.noreply.github.com>
2023-07-07 16:31:27 +0200

    split computations for lg height and lg position

--- src/fastga/models/geometry/geom_components/landing_gears/__init__.py
+++ src/fastga/models/geometry/geom_components/landing_gears/__init__.py
@@ -14,4 +14,5 @@ Estimation of landing gears geometry.
 #  You should have received a copy of the GNU General Public License
 #  along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
-from .compute_lg import ComputeLGGeometry
+from .compute_lg_height import ComputeLGHeight
+from .compute_lg_y_position import ComputeLGPosition
