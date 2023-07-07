a5e2e459c66fd611febcb2ae1ee4be35e11fac7f
Muhamad Abdul Aziz <79506671+Muhabdaziz@users.noreply.github.com>
2023-07-07 15:20:41 +0200

    Split computations for ht mac length, ht mac y, ht mac xpos local, and ht mac xpos from wing
    The test function is not modified in this case since they are tested based on each group (fd and fl).

---
+++ src/fastga/models/geometry/geom_components/ht/components/compute_ht_mac25_x_from_wing.py
@@ -0,0 +1,117 @@
+"""
+    Estimation of horizontal tail 25% mean aerodynamic chords x position from wing 
+    25% mean aerodynamic chord.
+"""
+
+#  This file is part of FAST-OAD_CS23 : A framework for rapid Overall Aircraft Design
+#  Copyright (C) 2022  ONERA & ISAE-SUPAERO
+#  FAST is free software: you can redistribute it and/or modify
+#  it under the terms of the GNU General Public License as published by
+#  the Free Software Foundation, either version 3 of the License, or
+#  (at your option) any later version.
+#  This program is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU General Public License for more details.
+#  You should have received a copy of the GNU General Public License
+#  along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+import numpy as np
+
+import openmdao.api as om
+
+
+class ComputeHTMacX25Wing(om.ExplicitComponent):
+    """
+    Compute x coordinate (from wing MAC .25) at 25% MAC of the horizontal tail.
+    """
+
+    def setup(self):
+
+        self.add_input("data:geometry:horizontal_tail:root:chord", val=np.nan, units="m")
+        self.add_input("data:geometry:fuselage:length", val=np.nan, units="m")
+        self.add_input("data:geometry:has_T_tail", val=np.nan)
+        self.add_input(
+            "data:geometry:horizontal_tail:MAC:at25percent:x:absolute", val=np.nan, units="m"
+        )
+        self.add_input("data:geometry:wing:MAC:at25percent:x", val=np.nan, units="m")
+        self.add_input(
+            "data:geometry:horizontal_tail:MAC:at25percent:x:local", val=np.nan, units="m"
+        )
+
+        self.add_output("data:geometry:horizontal_tail:MAC:at25percent:x:from_wingMAC25", units="m")
+
+        self.declare_partials(
+            "data:geometry:horizontal_tail:MAC:at25percent:x:from_wingMAC25",
+            [
+                "data:geometry:horizontal_tail:root:chord",
+                "data:geometry:horizontal_tail:MAC:at25percent:x:absolute",
+                "data:geometry:fuselage:length",
+                "data:geometry:wing:MAC:at25percent:x",
+                "data:geometry:horizontal_tail:MAC:at25percent:x:local",
+            ],
+            method="exact",
+        )
+
+    def compute(self, inputs, outputs, discrete_inputs=None, discrete_outputs=None):
+
+        root_chord = inputs["data:geometry:horizontal_tail:root:chord"]
+        fus_length = inputs["data:geometry:fuselage:length"]
+        tail_type = inputs["data:geometry:has_T_tail"]
+        x_ht = inputs["data:geometry:horizontal_tail:MAC:at25percent:x:absolute"]
+        x_wing25 = inputs["data:geometry:wing:MAC:at25percent:x"]
+        x0_ht = inputs["data:geometry:horizontal_tail:MAC:at25percent:x:local"]
+
+        if tail_type == 1.0:
+            ht_lp = (x_ht + x0_ht) - x_wing25
+        else:
+            ht_lp = (fus_length - root_chord + x0_ht) - x_wing25
+
+        outputs["data:geometry:horizontal_tail:MAC:at25percent:x:from_wingMAC25"] = ht_lp
+
+    def compute_partials(self, inputs, partials, discrete_inputs=None):
+
+        tail_type = inputs["data:geometry:has_T_tail"]
+
+        if tail_type == 1.0:
+            partials[
+                "data:geometry:horizontal_tail:MAC:at25percent:x:from_wingMAC25",
+                "data:geometry:horizontal_tail:root:chord",
+            ] = 0.0
+            partials[
+                "data:geometry:horizontal_tail:MAC:at25percent:x:from_wingMAC25",
+                "data:geometry:horizontal_tail:MAC:at25percent:x:absolute",
+            ] = 1.0
+            partials[
+                "data:geometry:horizontal_tail:MAC:at25percent:x:from_wingMAC25",
+                "data:geometry:fuselage:length",
+            ] = 0.0
+            partials[
+                "data:geometry:horizontal_tail:MAC:at25percent:x:from_wingMAC25",
+                "data:geometry:wing:MAC:at25percent:x",
+            ] = -1.0
+            partials[
+                "data:geometry:horizontal_tail:MAC:at25percent:x:from_wingMAC25",
+                "data:geometry:horizontal_tail:MAC:at25percent:x:local",
+            ] = 1.0
+        else:
+            partials[
+                "data:geometry:horizontal_tail:MAC:at25percent:x:from_wingMAC25",
+                "data:geometry:horizontal_tail:root:chord",
+            ] = -1.0
+            partials[
+                "data:geometry:horizontal_tail:MAC:at25percent:x:from_wingMAC25",
+                "data:geometry:horizontal_tail:MAC:at25percent:x:absolute",
+            ] = 0.0
+            partials[
+                "data:geometry:horizontal_tail:MAC:at25percent:x:from_wingMAC25",
+                "data:geometry:fuselage:length",
+            ] = 1.0
+            partials[
+                "data:geometry:horizontal_tail:MAC:at25percent:x:from_wingMAC25",
+                "data:geometry:wing:MAC:at25percent:x",
+            ] = -1.0
+            partials[
+                "data:geometry:horizontal_tail:MAC:at25percent:x:from_wingMAC25",
+                "data:geometry:horizontal_tail:MAC:at25percent:x:local",
+            ] = 1.0
--- src/fastga/models/geometry/geom_components/ht/components/compute_ht_mac_fd.py
+++ src/fastga/models/geometry/geom_components/ht/components/compute_ht_mac_fd.py
@@ -1,6 +1,8 @@
 """
-Estimation of horizontal tail geometry (components).
+    Estimation of horizontal tail mean aerodynamic chord properties based on 
+    (F)ixed tail (D)istance.
 """
+
 #  This file is part of FAST-OAD_CS23 : A framework for rapid Overall Aircraft Design
 #  Copyright (C) 2022  ONERA & ISAE-SUPAERO
 #  FAST is free software: you can redistribute it and/or modify
@@ -14,15 +16,28 @@ Estimation of horizontal tail geometry (components).
 #  You should have received a copy of the GNU General Public License
 #  along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
-from .compute_ht_distance import ComputeHTDistance
-from .compute_ht_mac_fd import ComputeHTMacFD
-from .compute_ht_mac_fl import ComputeHTMacFL
-from .compute_ht_sweep_0 import ComputeHTSweep0
-from .compute_ht_sweep_50 import ComputeHTSweep50
-from .compute_ht_sweep_100 import ComputeHTSweep100
-from .compute_ht_wet_area import ComputeHTWetArea
-from .compute_ht_efficiency import ComputeHTEfficiency
-from .compute_ht_volume_coefficient import ComputeHTVolumeCoefficient
-from .compute_ht_span import ComputeHTSpan
-from .compute_ht_chord_root import ComputeHTRootChord
-from .compute_ht_chord_tip import ComputeHTTipChord
+import openmdao.api as om
+
+from .compute_ht_mac_length import ComputeHTMacLength
+from .compute_ht_mac_y import ComputeHTMacY
+from .compute_ht_mac25_x_local import ComputeHTMacX25
+
+
+class ComputeHTMacFD(om.Group):
+    def setup(self):
+
+        self.add_subsystem(
+            "comp_ht_mac_length_fd",
+            ComputeHTMacLength(),
+            promotes=["*"],
+        )
+        self.add_subsystem(
+            "comp_ht_mac_x25_fd",
+            ComputeHTMacX25(),
+            promotes=["*"],
+        )
+        self.add_subsystem(
+            "comp_ht_mac_y_fd",
+            ComputeHTMacY(),
+            promotes=["*"],
+        )
