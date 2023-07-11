4725d0f841b661f550657b5971c1481dfce138d1
Biel Galiot Pérez <39485660+bielgaliot@users.noreply.github.com>
2023-07-11 11:46:46 +0200

    First buggy version of feedback extractor using times. To be solved: congig_optimizer now jumping loops even if better config has not been found for current loop. try with full problem. To be added: switch between using times or nº BLC, Optimize if INFO=False

--- FAST-OAD_notebooks/fast-oad-cs23/tutorial/feedback_extractor.py
+++ FAST-OAD_notebooks/fast-oad-cs23/tutorial/feedback_extractor.py
@@ -1,5 +1,6 @@
-def feedback_extractor(model_data, INFO = False):
-
+def feedback_extractor(model_data, config_dictionary, INFO = False):
+#TODO: if info is false, only compute the number, not save every src tgt pair
+#TODO: compute time
     import time
 
     #INFO: used to toggle on and off the printing of feedback loops and other info
@@ -34,10 +35,66 @@ def feedback_extractor(model_data, INFO = False):
             result.append((src_word, tgt_word))
 
         return result
+    
+    def extract_module(data):
+        result = []
+        for entry in data:
+            src_value = entry['src']
+            tgt_value = entry['tgt']
 
-    # Define relative path
-    #WORK_FOLDER_PATH = "workdir"
-    #CONFIGURATION_FILE = pth.join(WORK_FOLDER_PATH, "oad_process_test.yml")
+            src_parts = src_value.split('.')
+            tgt_parts = tgt_value.split('.')
+
+            src_word = src_parts[1] if len(src_parts) > 1 and src_parts[0] != 'fastoad_shaper' else ''
+            tgt_word = tgt_parts[1] if len(tgt_parts) > 1 and tgt_parts[0] != 'fastoad_shaper' else ''
+
+            if src_word != '' and tgt_word != '':
+                result.append((src_word, tgt_word))
+        return result
+
+    def time_modules(feedback_list):
+        modules_times = {
+            "geometry": 1.8936853075027467,
+            "aerodynamics_lowspeed" : 2,
+            "aerodynamics_highspeed" : 2,
+            "weight" : 3,
+            "performance" : 1,
+            "hq" : 1.5,
+            "mtow" : 0.5,
+            "wing_position" : 3,
+            "wing_area" : 2,
+        }
+
+        #create config file for each module separately, run problem and store time
+        #find how many times they run
+        modules_in_feedback = extract_module(feedback_list)
+        keys_order = list(config_dictionary.keys())
+        
+        rerun_counts = {key: 0 for key in keys_order}  # Initialize a dictionary to store rerun counts
+
+        #count modules that have to be rerun
+        for pair in modules_in_feedback:
+            source, target = pair
+            start_index = keys_order.index(source)  # Find the index of the source module
+            end_index = keys_order.index(target)  # Find the index of the target module
+            
+            rerun_modules = keys_order[end_index:start_index+1]  # Extract the modules that need to be rerun
+            
+            for module in rerun_modules:
+                rerun_counts[module] += 1  # Increment the rerun count (dict) for each module
+
+        #Compute total time knowing how many reruns each module has:
+        total_time = 0
+        for module, count in rerun_counts.items():
+            total_time += count * modules_times[module]
+
+        #TODO: multiply the times of the modules given by modules_times by the counts in module counts
+
+        #return score as sum (time*times they run).
+        return total_time
+
+
+
 
     #retrieves the dictionary for models and their sub-models and returns as an ordered list of the full "paths" to the variables
     ordered_vars = process_variables(model_data['tree']['children'])
@@ -57,8 +114,18 @@ def feedback_extractor(model_data, INFO = False):
     #extract the BLCs from the variables
     list_of_BLC_in_feedback = extract_BLC(result_list)
     list_of_BLC_in_feedback = [(a, b) for (a, b) in list_of_BLC_in_feedback if 'fastoad_shaper' not in (a, b)] # remove fastoad_shaper from feedback counts
+    
 
 
+
+
+    score = time_modules(result_list)
+
+
+
+
+
+    print('Score: ', score)
     if INFO:
         print('\n There are', len(list_of_BLC_in_feedback), 'feedback connections \n')
 
@@ -72,12 +139,9 @@ def feedback_extractor(model_data, INFO = False):
                 loop_counter += 1
         print("\n")
         print('\n Time taken', time.time() - start, 'seconds')
-
-    print('Feedbacks: ', len(list_of_BLC_in_feedback))
     
-    return len(list_of_BLC_in_feedback)
+    return score
 
 
 
 
-#TODO: if info is false, only compute the number, not save every src tgt pair
--- FAST-OAD_notebooks/fast-oad-cs23/tutorial/feedback_extractor_test.py
+++ FAST-OAD_notebooks/fast-oad-cs23/tutorial/feedback_extractor_test.py
@@ -5,11 +5,13 @@ import os.path as pth
 import os
 import sys
 import time
+import yaml
+
 from fastoad.io.configuration import FASTOADProblemConfigurator
 from openmdao.visualization.n2_viewer.n2_viewer import _get_viewer_data
 from openmdao.utils.om_warnings import issue_warning
 
-INFO = True
+INFO = False
     #INFO: used to toggle on and off the printing of feedback loops and other info
 if INFO: start = time.time()
 
@@ -43,6 +45,24 @@ def extract_BLC(data): #extracts the Bottom level components to which the feedba
 
     return result
 
+
+def extract_module(data):
+    result = []
+    for entry in data:
+        src_value = entry['src']
+        tgt_value = entry['tgt']
+
+        src_parts = src_value.split('.')
+        tgt_parts = tgt_value.split('.')
+
+        src_word = src_parts[1] if len(src_parts) > 1 and src_parts[0] != 'fastoad_shaper' else ''
+        tgt_word = tgt_parts[1] if len(tgt_parts) > 1 and tgt_parts[0] != 'fastoad_shaper' else ''
+
+        if src_word != '' and tgt_word != '':
+            result.append((src_word, tgt_word))
+
+    return result
+
 # Define relative path
 WORK_FOLDER_PATH = "workdir"
 CONFIGURATION_FILE = pth.join(WORK_FOLDER_PATH, "oad_process_test.yml")
@@ -51,6 +71,24 @@ conf = FASTOADProblemConfigurator(CONFIGURATION_FILE)
 problem = conf.get_problem()
 problem.setup()
 problem.final_setup()
+
+with open(CONFIGURATION_FILE, 'r') as file:
+
+        # Load the YAML contents
+        yaml_data = yaml.safe_load(file)
+
+        # Get the structure under 'model: aircraft_sizing'
+        try:
+            yaml_data['model']['aircraft_sizing']
+        except:
+            sys.exit("\n Error: Configuration optimizer assumes existence of aircraft_sizing inside model .yml config file \n")
+
+        yaml_data['model']['aircraft_sizing'].pop('nonlinear_solver', None)
+        yaml_data['model']['aircraft_sizing'].pop('linear_solver', None)
+        aircraft_sizing_data = yaml_data['model']['aircraft_sizing']
+
+keys = aircraft_sizing_data.keys()
+print(keys)
 
 #convert problem to dictionary
 case_id=None
@@ -76,11 +114,15 @@ for data in connections_list:
         result_list.append(data)
         distance_of_BLC.append(src_position - tgt_position) #register also how far apart they are
 
-
+#print(result_list)
 #extract the BLCs from the variables
 list_of_BLC_in_feedback = extract_BLC(result_list)
 list_of_BLC_in_feedback = [(a, b) for (a, b) in list_of_BLC_in_feedback if 'fastoad_shaper' not in (a, b)] # remove fastoad_shaper from feedback counts
 
+#extract the modules from the variables
+list_of_modules_in_feedback =  extract_module(result_list)
+
+print(list_of_modules_in_feedback)
 
 if INFO:
     print('\n There are', len(list_of_BLC_in_feedback), 'feedback connections \n')
