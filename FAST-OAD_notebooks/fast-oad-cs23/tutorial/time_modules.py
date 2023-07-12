d33474c1c6841591336aacdbc7f1040028ff4ed6
Biel Galiot Pérez <39485660+bielgaliot@users.noreply.github.com>
2023-07-12 16:10:59 +0200

    time_modules working as expected, except for weight. It cannot run on its own no matter the source file

--- FAST-OAD_notebooks/fast-oad-cs23/tutorial/time_modules.py
+++ FAST-OAD_notebooks/fast-oad-cs23/tutorial/time_modules.py
@@ -12,9 +12,20 @@ def time_modules(config_dictionary, ORIGINAL_CONFIGURATION_FILE):
     import time
     import yaml
     import shutil
+    import re
+
+    def find_id_value(dictionary):
+        if 'id' in dictionary:
+            return dictionary['id']
+        else:
+            for value in dictionary.values():
+                if isinstance(value, dict):
+                    id_value = find_id_value(value)
+                    if id_value is not None:
+                        return id_value
+        return None
 
     # Define relative path
-    DATA_FOLDER_PATH = "data"
     WORK_FOLDER_PATH = "workdir"
 
     # Obtaining the modules for this version of fastoad
@@ -23,49 +34,67 @@ def time_modules(config_dictionary, ORIGINAL_CONFIGURATION_FILE):
     result = subprocess.run(command, shell=True, capture_output=True, text=True)
     # Access the output using the 'stdout' attribute
     list_modules = result.stdout
-    print(type(list_modules))
+    module_path_pairs = re.findall(r'\|\s*([^|]+)\s*\|\s*([^|]+)\s*\|', list_modules)
+    module_path_pairs = [(module.strip(), path.strip()) for module, path in module_path_pairs]
+
 
     module_times = dict.fromkeys(config_dictionary.keys(), 0)
-    
-    for module in config_dictionary.keys(): #iterate over each module present in the configuration file we are trying to optimize
-        
-        #generate the configuration file with single module
-        CONFIGURATION_FILE = pth.join(WORK_FOLDER_PATH, "config_opti_tmp", str(module))
 
-        shutil.copyfile(ORIGINAL_CONFIGURATION_FILE, CONFIGURATION_FILE)
-        with open(CONFIGURATION_FILE, 'r') as file:
+
+    for module in config_dictionary.keys(): #iterate over each module present in the configuration file we are trying to optimize
+
+        #generate the configuration file with single module (no optimization and no propeller)
+        NEW_CONFIGURATION_FILE = pth.join(WORK_FOLDER_PATH, 'config_opti_tmp', str(module))
+        if not pth.isdir(NEW_CONFIGURATION_FILE):
+            os.makedirs(NEW_CONFIGURATION_FILE)
+        shutil.copy(ORIGINAL_CONFIGURATION_FILE, NEW_CONFIGURATION_FILE)
+        NEW_CONFIGURATION_FILE = pth.join(NEW_CONFIGURATION_FILE, os.path.basename(ORIGINAL_CONFIGURATION_FILE))
+        with open(NEW_CONFIGURATION_FILE, 'r') as file:
                 data = yaml.safe_load(file)
-                data['model']['aircraft_sizing'] = config_dictionary[module] 
-        with open(CONFIGURATION_FILE, 'w') as file:
-                if data['optimization']: del data['optimization'] 
+                #Deletes keys inside aircraft sizing except the solvers, if present and the module we want to time
+                keys_to_delete = [key for key in data['model']['aircraft_sizing'] if key not in ['linear_solver', 'nonlinear_solver', module]]
+                for key in keys_to_delete:
+                    del data['model']['aircraft_sizing'][key]
+                #data['model']['aircraft_sizing'] = config_dictionary[module] #this is simpler but eliminates the specified solvers.
+        with open(NEW_CONFIGURATION_FILE, 'w') as file:
+                if 'optimization' in data: del data['optimization'] #no optimization
+                if 'propeller' in data['model']: del data['model']['propeller'] #no propeller
                 yaml.dump(data, file, default_flow_style=False, sort_keys=False)
     
+        #Fetch the appropriate SOURCE file from unitary_tests of each module, with the paths to the modules listed by the fastoad list_modules ran before
         ################################
-        path_to_source = #parse list_modules to find the location of source files in: route-to-module just before  aerodynamics_low_speed.py file/unitary_tests/data/beechcraft_76.xml
-        ###############################
-        SOURCE_FILE = pth.join(DATA_FOLDER_PATH, "COMPLETEEEEEEE")
+        #Find the id or ids of the module in question
+        id_of_module = find_id_value(config_dictionary[module])
 
-        api_cs25.generate_inputs(CONFIGURATION_FILE, SOURCE_FILE, overwrite=True)
+        #Find the path to its folder
+        path_to_id_of_module = next((pair[1] for pair in module_path_pairs if id_of_module in pair[0]), None)
+        #Find the path to its unitary tests folder, which contains the source file
+        index = path_to_id_of_module.index("/models")  # Find the index of "/models"
+        next_folder_index = path_to_id_of_module.find("/", index + len("/models") + 1) # Find the index of the folder immediately after models
+             
+        if module != 'weight':
+            NEW_SOURCE_FILE = path_to_id_of_module[:next_folder_index] + '/unitary_tests/data/beechcraft_76.xml' #add the path to the source file used for the unitary test of module being timed
+   
+            ###############################
 
-        executions_time = []
+            api_cs25.generate_inputs(NEW_CONFIGURATION_FILE, NEW_SOURCE_FILE, overwrite=True)
 
-        for _ in range(20): #run them individually 20 times, to have a good average
+            executions_time = []
 
-            starting = time.time()
-            eval_problem = api_cs25.evaluate_problem(CONFIGURATION_FILE, overwrite=True)
+            print('\n Starting timings of: ', module)
+            for _ in range(20): #run them individually 20 times, to have a good average
 
-            print('\n Problem ran in ', time.time() - starting , ' seconds \n')
-            executions_time.append(time.time() - starting)
+                starting = time.time()
+                eval_problem = api_cs25.evaluate_problem(NEW_CONFIGURATION_FILE, overwrite=True)
 
-            """try:
-                os.remove(WORK_FOLDER_PATH, "problem_outputs.xml")
-            except:
-                pass"""
+                print('\n Problem ran in ', time.time() - starting , ' seconds \n')
+                executions_time.append(time.time() - starting)
 
-        module_times[module] = sum(executions_time)/len(executions_time)
-
-    
-
+            module_times[module] = sum(executions_time)/len(executions_time)
+            print('\n Average time of ', module, ': ', module_times[module], ' seconds')
+        else:
+            module_times['weight'] = 3
+         
+    #remove all temporary config files created for unitary timing
+    shutil.rmtree(pth.join(WORK_FOLDER_PATH, 'config_opti_tmp'))
     return  module_times
-
-    #remove all created yml files
