5cbe3d8e5db249699757dbf5c741aadffba13868
johnathanasd <93048.87051.ko@gmail.com>
2023-07-13 17:29:00 +0800

    amendment for the explanation plot

--- src/fastga/models/aerodynamics/external/vlm/vlm.py
+++ src/fastga/models/aerodynamics/external/vlm/vlm.py
@@ -163,15 +163,15 @@ class VLMSimpleGeometry(om.ExplicitComponent):
         cl_0_htp, cl_X_htp, cl_alpha_htp, cl_alpha_htp_isolated, y_vector_htp, cl_vector_htp,
         coef_k_htp parameters.
         
-               ^
-             y |                Points defining the panel
-               |                are named clockwise. A(x_1,y_1), B(x_2,y_2), P(x_c,y_c)
-        P3--B--|-----P4
-        |  |   |     |
-        |  |   |     |
-        T1 |   +--P--T2---->
-        |  |         |     x
-        |  |         |
+                ^
+              y |                Points defining the panel
+                |                are named clockwise. A(x_1,y_1), B(x_2,y_2), P(x_c,y_c)
+        P3--B---|-----P4
+        |   |   |     |
+        |   |   |     |
+        T1  |   +--P--T2---->
+        |   |         |     x
+        |   |         |
         P2--A--------P1
         """
 
