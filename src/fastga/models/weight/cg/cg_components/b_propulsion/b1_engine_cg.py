aee5eb7e987e853bdf403270a47b9554c4f17273
Muhamad Abdul Aziz <79506671+Muhabdaziz@users.noreply.github.com>
2023-07-27 08:27:59 +0200

    Add derivative computation for engine cg

--- src/fastga/models/weight/cg/cg_components/b_propulsion/b1_engine_cg.py
+++ src/fastga/models/weight/cg/cg_components/b_propulsion/b1_engine_cg.py
@@ -61,7 +61,7 @@ class ComputeEngineCG(om.ExplicitComponent):
                 "data:geometry:propulsion:nacelle:y",
                 "data:geometry:propeller:depth",
             ],
-            method="fd",
+            method="exact",
         )
 
     def compute(self, inputs, outputs, discrete_inputs=None, discrete_outputs=None):
@@ -109,3 +109,100 @@ class ComputeEngineCG(om.ExplicitComponent):
             )
 
         outputs["data:weight:propulsion:engine:CG:x"] = x_cg_b1
+
+    def compute_partials(self, inputs, partials, discrete_inputs=None):
+
+        prop_layout = inputs["data:geometry:propulsion:engine:layout"]
+        engine_count_pre_wing = inputs["data:geometry:propulsion:engine:count"] / 2.0
+        y2_wing = inputs["data:geometry:wing:root:y"]
+        l2_wing = inputs["data:geometry:wing:root:chord"]
+        y4_wing = inputs["data:geometry:wing:tip:y"]
+        l4_wing = inputs["data:geometry:wing:tip:chord"]
+        y_nacelle_array = inputs["data:geometry:propulsion:nacelle:y"]
+
+        if prop_layout == 1.0:
+            d_x_cg_b1_d_y2_wing = 0.0
+            d_x_cg_b1_d_l2_wing = 0.0
+            d_x_cg_b1_d_y4_wing = 0.0
+            d_x_cg_b1_d_l4_wing = 0.0
+            d_x_cg_b1_d_y_nacelle = np.copy(y_nacelle_array)
+
+            for idx, y_nacelle in enumerate(y_nacelle_array):
+                if y_nacelle > y2_wing:  # Nacelle in the tapered part of the wing
+                    d_x_nacelle_cg_d_y2_wing = (
+                        -0.05
+                        * (l2_wing - l4_wing)
+                        * (y4_wing - y_nacelle)
+                        / (y4_wing - y2_wing) ** 2.0
+                    )
+                    d_x_nacelle_cg_d_l2_wing = -0.05 * (y4_wing - y_nacelle) / (y4_wing - y2_wing)
+                    d_x_nacelle_cg_d_y4_wing = (
+                        -0.05
+                        * (l2_wing - l4_wing)
+                        * ((y4_wing - y2_wing) - (y4_wing - y_nacelle))
+                        / (y4_wing - y2_wing) ** 2.0
+                    )
+                    d_x_nacelle_cg_d_l4_wing = -0.05 * (
+                        1 - (y4_wing - y_nacelle) / (y4_wing - y2_wing)
+                    )
+                    d_x_cg_b1_d_y_nacelle[idx] = -0.05 * (
+                        -(l2_wing - l4_wing) / (y4_wing - y2_wing)
+                    )
+
+                else:  # Nacelle in the straight part of the wing
+                    d_x_nacelle_cg_d_y2_wing = 0.0
+                    d_x_nacelle_cg_d_l2_wing = -0.05
+                    d_x_nacelle_cg_d_y4_wing = 0.0
+                    d_x_nacelle_cg_d_l4_wing = 0.0
+                    d_x_cg_b1_d_y_nacelle[idx] = 0.0
+
+                d_x_cg_b1_d_y2_wing += d_x_nacelle_cg_d_y2_wing / engine_count_pre_wing
+                d_x_cg_b1_d_l2_wing += d_x_nacelle_cg_d_l2_wing / engine_count_pre_wing
+                d_x_cg_b1_d_y4_wing += d_x_nacelle_cg_d_y4_wing / engine_count_pre_wing
+                d_x_cg_b1_d_l4_wing += d_x_nacelle_cg_d_l4_wing / engine_count_pre_wing
+
+            partials[
+                "data:weight:propulsion:engine:CG:x", "data:geometry:wing:root:y"
+            ] = d_x_cg_b1_d_y2_wing
+            partials[
+                "data:weight:propulsion:engine:CG:x", "data:geometry:wing:root:chord"
+            ] = d_x_cg_b1_d_l2_wing
+            partials[
+                "data:weight:propulsion:engine:CG:x", "data:geometry:wing:tip:y"
+            ] = d_x_cg_b1_d_y4_wing
+            partials[
+                "data:weight:propulsion:engine:CG:x", "data:geometry:wing:tip:chord"
+            ] = d_x_cg_b1_d_l4_wing
+            partials[
+                "data:weight:propulsion:engine:CG:x", "data:geometry:propulsion:nacelle:length"
+            ] = -0.4
+            partials[
+                "data:weight:propulsion:engine:CG:x", "data:geometry:propulsion:nacelle:y"
+            ] = d_x_cg_b1_d_y_nacelle
+            partials["data:weight:propulsion:engine:CG:x", "data:geometry:propeller:depth"] = 0.0
+
+        elif prop_layout == 2.0:
+            partials["data:weight:propulsion:engine:CG:x", "data:geometry:wing:root:y"] = 0.0
+            partials["data:weight:propulsion:engine:CG:x", "data:geometry:wing:root:chord"] = 0.0
+            partials["data:weight:propulsion:engine:CG:x", "data:geometry:wing:tip:y"] = 0.0
+            partials["data:weight:propulsion:engine:CG:x", "data:geometry:wing:tip:chord"] = 0.0
+            partials[
+                "data:weight:propulsion:engine:CG:x", "data:geometry:propulsion:nacelle:length"
+            ] = -0.4
+            partials[
+                "data:weight:propulsion:engine:CG:x", "data:geometry:propulsion:nacelle:y"
+            ] = 0.0
+            partials["data:weight:propulsion:engine:CG:x", "data:geometry:propeller:depth"] = 0.0
+
+        else:
+            partials["data:weight:propulsion:engine:CG:x", "data:geometry:wing:root:y"] = 0.0
+            partials["data:weight:propulsion:engine:CG:x", "data:geometry:wing:root:chord"] = 0.0
+            partials["data:weight:propulsion:engine:CG:x", "data:geometry:wing:tip:y"] = 0.0
+            partials["data:weight:propulsion:engine:CG:x", "data:geometry:wing:tip:chord"] = 0.0
+            partials[
+                "data:weight:propulsion:engine:CG:x", "data:geometry:propulsion:nacelle:length"
+            ] = 0.6
+            partials[
+                "data:weight:propulsion:engine:CG:x", "data:geometry:propulsion:nacelle:y"
+            ] = 0.0
+            partials["data:weight:propulsion:engine:CG:x", "data:geometry:propeller:depth"] = 1.0
