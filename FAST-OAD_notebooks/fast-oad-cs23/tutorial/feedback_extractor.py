16ad1b1a7a40e6a722bc5ed8b3d311b2d92d3f0e
Biel Galiot Pérez <39485660+bielgaliot@users.noreply.github.com>
2023-07-31 11:26:07 +0200

    Ran black on files

--- FAST-OAD_notebooks/fast-oad-cs23/tutorial/feedback_extractor.py
+++ FAST-OAD_notebooks/fast-oad-cs23/tutorial/feedback_extractor.py
@@ -1,178 +1,197 @@
-def feedback_extractor(model_data, config_dictionary, CONFIGURATION_FILE, score_criteria, WORK_FOLDER_PATH,  INFO = False):
-#TODO: if info is false, only compute the number, not save every src tgt pair
+def feedback_extractor(
+    model_data, config_dictionary, CONFIGURATION_FILE, score_criteria, WORK_FOLDER_PATH, INFO=False
+):
+    # TODO: if info is false, only compute the number, not save every src tgt pair
 
     import time
     from time_modules import time_modules
     import sys
-    #INFO: used to toggle on and off the printing of feedback loops and other info
-    if INFO: start = time.time()
+
+    # INFO: used to toggle on and off the printing of feedback loops and other info
+    if INFO:
+        start = time.time()
     import functools
     import os
     import json
 
-    #definition of the function to output an n2-ordered list of all the variables in the problem
-    #The dictionary is actually an alternation of lists of dicts where eahc dicht has the key 'children', which contains a list of dicts, and so on
-    def process_variables(children_list, prefix=''):
+    # definition of the function to output an n2-ordered list of all the variables in the problem
+    # The dictionary is actually an alternation of lists of dicts where eahc dicht has the key 'children', which contains a list of dicts, and so on
+    def process_variables(children_list, prefix=""):
         variables = []
         for child in children_list:
-            if 'name' in child:
-                var_name = prefix + child['name']
+            if "name" in child:
+                var_name = prefix + child["name"]
                 variables.append(var_name)
-            if 'children' in child:
-                child_prefix = prefix + child['name'] + '.'
-                variables.extend(process_variables(child['children'], child_prefix)) #recursive call of the function to reach the last 
+            if "children" in child:
+                child_prefix = prefix + child["name"] + "."
+                variables.extend(
+                    process_variables(child["children"], child_prefix)
+                )  # recursive call of the function to reach the last
         return variables
 
-
-    def extract_BLC(data): #extracts the Bottom level components to which the feedbacking variables belong
+    def extract_BLC(
+        data,
+    ):  # extracts the Bottom level components to which the feedbacking variables belong
         result = []
         for entry in data:
-            src_value = entry['src']
-            tgt_value = entry['tgt']
+            src_value = entry["src"]
+            tgt_value = entry["tgt"]
 
-            src_parts = src_value.split('.')
-            tgt_parts = tgt_value.split('.')
+            src_parts = src_value.split(".")
+            tgt_parts = tgt_value.split(".")
 
-            src_word = src_parts[-2] if len(src_parts) >= 2 else ''
-            tgt_word = tgt_parts[-2] if len(tgt_parts) >= 2 else ''
+            src_word = src_parts[-2] if len(src_parts) >= 2 else ""
+            tgt_word = tgt_parts[-2] if len(tgt_parts) >= 2 else ""
 
             result.append((src_word, tgt_word))
 
         return result
-    
+
     def extract_module(data):
         result = []
         for entry in data:
-            src_value = entry['src']
-            tgt_value = entry['tgt']
+            src_value = entry["src"]
+            tgt_value = entry["tgt"]
 
-            src_parts = src_value.split('.')
-            tgt_parts = tgt_value.split('.')
+            src_parts = src_value.split(".")
+            tgt_parts = tgt_value.split(".")
 
-            src_word = src_parts[1] if len(src_parts) > 1 and src_parts[0] != 'fastoad_shaper' else ''
-            tgt_word = tgt_parts[1] if len(tgt_parts) > 1 and tgt_parts[0] != 'fastoad_shaper' else ''
+            src_word = (
+                src_parts[1] if len(src_parts) > 1 and src_parts[0] != "fastoad_shaper" else ""
+            )
+            tgt_word = (
+                tgt_parts[1] if len(tgt_parts) > 1 and tgt_parts[0] != "fastoad_shaper" else ""
+            )
 
-            if src_word != '' and tgt_word != '':
+            if src_word != "" and tgt_word != "":
                 result.append((src_word, tgt_word))
         return result
 
     def total_time_of_modules(score_criteria, WORK_FOLDER_PATH):
-        if score_criteria == 'use_time': #use pre-ran times for each individual module
-            if os.path.exists('tmp_saved_single_module_timings.txt'):
-                with open('tmp_saved_single_module_timings.txt', 'r') as file:
+        if score_criteria == "use_time":  # use pre-ran times for each individual module
+            if os.path.exists("tmp_saved_single_module_timings.txt"):
+                with open("tmp_saved_single_module_timings.txt", "r") as file:
                     module_times = json.loads(file.read())
             else:
-                print('\n Using pre-run module times. These may be different for your machine. It is recommended to run first with the option: compute_time')
+                print(
+                    "\n Using pre-run module times. These may be different for your machine. It is recommended to run first with the option: compute_time"
+                )
                 modules_times = {
                     "geometry": 1.7216651797294618,
-                    "aerodynamics_lowspeed" : 2.5686846733093263,
-                    "aerodynamics_highspeed" : 2.165796732902527,
-                    "weight" : 3.7698839783668516,
-                    "performance" : 24.092622423171996,
-                    "hq" : 13.180185759067536,
-                    "mtow" : 1.0099695563316344,
-                    "wing_position" : 1.0157811045646667,
-                    "wing_area" : 1.1173424243927002,
+                    "aerodynamics_lowspeed": 2.5686846733093263,
+                    "aerodynamics_highspeed": 2.165796732902527,
+                    "weight": 3.7698839783668516,
+                    "performance": 24.092622423171996,
+                    "hq": 13.180185759067536,
+                    "mtow": 1.0099695563316344,
+                    "wing_position": 1.0157811045646667,
+                    "wing_area": 1.1173424243927002,
                 }
-        else: #compute modules times for your particular machine, solver, etc.
+        else:  # compute modules times for your particular machine, solver, etc.
             modules_times = time_modules(config_dictionary, CONFIGURATION_FILE, WORK_FOLDER_PATH)
-        
 
         return modules_times
 
     def run_once(func):
         @functools.wraps(func)
         def wrapper(*args, **kwargs):
-            if not os.path.exists('tmp_saved_single_module_timings.txt'):
+            if not os.path.exists("tmp_saved_single_module_timings.txt"):
                 result = func(*args, **kwargs)
-                with open('tmp_saved_single_module_timings.txt', 'w') as f:
+                with open("tmp_saved_single_module_timings.txt", "w") as f:
                     f.write(json.dumps(result))
                 return result
             else:
-                with open('tmp_saved_single_module_timings.txt', 'r') as f:
+                with open("tmp_saved_single_module_timings.txt", "r") as f:
                     result = json.load(f)
                 return result
 
         return wrapper
-    #apply decorator
-    time_modules = run_once(time_modules) #Ensures that we only compute the time of the individual modules once
 
+    # apply decorator
+    time_modules = run_once(
+        time_modules
+    )  # Ensures that we only compute the time of the individual modules once
 
-#############################################
-#End of the functions
-#############################################
+    #############################################
+    # End of the functions
+    #############################################
 
-    #retrieves the dictionary for models and their sub-models and returns as an ordered list of the full "paths" to the variables
-    ordered_vars = process_variables(model_data['tree']['children'])
+    # retrieves the dictionary for models and their sub-models and returns as an ordered list of the full "paths" to the variables
+    ordered_vars = process_variables(model_data["tree"]["children"])
 
-    #extracts connections for which the target is above the source, in the ordered list of variables
-    connections_list = model_data['connections_list']
+    # extracts connections for which the target is above the source, in the ordered list of variables
+    connections_list = model_data["connections_list"]
     result_list = []
     distance_of_BLC = []
     for data in connections_list:
-        src_position = ordered_vars.index(data['src'])
-        tgt_position = ordered_vars.index(data['tgt'])
+        src_position = ordered_vars.index(data["src"])
+        tgt_position = ordered_vars.index(data["tgt"])
         if src_position > tgt_position:
             result_list.append(data)
-            distance_of_BLC.append(src_position - tgt_position) #register also how far apart they are
+            distance_of_BLC.append(
+                src_position - tgt_position
+            )  # register also how far apart they are
 
-
-    #extract the BLCs from the variables
+    # extract the BLCs from the variables
     list_of_BLC_in_feedback = extract_BLC(result_list)
-    list_of_BLC_in_feedback = [(a, b) for (a, b) in list_of_BLC_in_feedback if 'fastoad_shaper' not in (a, b)] # remove fastoad_shaper from feedback counts
-    
-    if score_criteria == 'compute_time' or score_criteria == 'use_time':
+    list_of_BLC_in_feedback = [
+        (a, b) for (a, b) in list_of_BLC_in_feedback if "fastoad_shaper" not in (a, b)
+    ]  # remove fastoad_shaper from feedback counts
 
-        modules_times = total_time_of_modules(score_criteria, WORK_FOLDER_PATH) 
-      
+    if score_criteria == "compute_time" or score_criteria == "use_time":
 
-        #find how many times they run
+        modules_times = total_time_of_modules(score_criteria, WORK_FOLDER_PATH)
+
+        # find how many times they run
         modules_in_feedback = extract_module(result_list)
         keys_order = list(config_dictionary.keys())
-        
-        rerun_counts = {key: 0 for key in keys_order}  # Initialize a dictionary to store rerun counts
 
-        #count modules that have to be rerun
+        rerun_counts = {
+            key: 0 for key in keys_order
+        }  # Initialize a dictionary to store rerun counts
+
+        # count modules that have to be rerun
         for pair in modules_in_feedback:
             source, target = pair
             start_index = keys_order.index(source)  # Find the index of the source module
             end_index = keys_order.index(target)  # Find the index of the target module
-            
-            rerun_modules = keys_order[end_index:start_index+1]  # Extract the modules that need to be rerun
-            
+
+            rerun_modules = keys_order[
+                end_index : start_index + 1
+            ]  # Extract the modules that need to be rerun
+
             for module in rerun_modules:
                 rerun_counts[module] += 1  # Increment the rerun count (dict) for each module
 
-        #Compute total time knowing how many reruns each module has:
+        # Compute total time knowing how many reruns each module has:
         total_time = 0
         for module, count in rerun_counts.items():
             total_time += count * modules_times[module]
         score = total_time
 
-    elif score_criteria == 'count_feedbacks':
-        score = len(list_of_BLC_in_feedback) 
+    elif score_criteria == "count_feedbacks":
+        score = len(list_of_BLC_in_feedback)
     else:
-        sys.exit('\nScore criteria not valid. Please choose compute_time, use_time or count_feedbacks')
+        sys.exit(
+            "\nScore criteria not valid. Please choose compute_time, use_time or count_feedbacks"
+        )
 
-
-    print('Score: ', score)
+    print("Score: ", score)
 
     if INFO:
-        print('\n There are', len(list_of_BLC_in_feedback), 'feedback connections \n')
+        print("\n There are", len(list_of_BLC_in_feedback), "feedback connections \n")
 
-        #Prints the source and target of the feedback loop, along with the distance (in number of BLCs) that separates them
+        # Prints the source and target of the feedback loop, along with the distance (in number of BLCs) that separates them
         print("Feedback loops:")
         loop_counter = 1
         loop_set = set()
         for pair in list_of_BLC_in_feedback:
-                loop_set.add(pair)
-                print(f"{loop_counter}. {pair[0]} -> {pair[1]} | {distance_of_BLC[loop_counter-1]} BLCs")
-                loop_counter += 1
+            loop_set.add(pair)
+            print(
+                f"{loop_counter}. {pair[0]} -> {pair[1]} | {distance_of_BLC[loop_counter-1]} BLCs"
+            )
+            loop_counter += 1
         print("\n")
-        print('\n Time taken', time.time() - start, 'seconds')
-    
+        print("\n Time taken", time.time() - start, "seconds")
+
     return score
-
-
-
-
--- FAST-OAD_notebooks/fast-oad-cs23/tutorial/time_modules.py
+++ FAST-OAD_notebooks/fast-oad-cs23/tutorial/time_modules.py
@@ -4,7 +4,8 @@ def time_modules(config_dictionary, ORIGINAL_CONFIGURATION_FILE, WORK_FOLDER_PAT
     import os
     import openmdao.api as om
     from fastoad import api as api_cs25
-    #from fastga.command import api as api_cs23
+
+    # from fastga.command import api as api_cs23
     import subprocess
     import time
     import yaml
@@ -15,81 +16,103 @@ def time_modules(config_dictionary, ORIGINAL_CONFIGURATION_FILE, WORK_FOLDER_PAT
     import xml.etree.ElementTree as ET
     from configuration_optimizer import find_id_value
 
-    # Define relative path
-    #WORK_FOLDER_PATH = "workdir" ##########################################TODO: ELIMINATE FROM HERE, pass as argument
-
     # Obtaining the modules for this version of fastoad
     command = "fastoad list_modules"
     # Run the command and capture the output
     result = subprocess.run(command, shell=True, capture_output=True, text=True)
     # Access the output using the 'stdout' attribute
     list_modules = result.stdout
-    module_path_pairs = re.findall(r'\|\s*([^|]+)\s*\|\s*([^|]+)\s*\|', list_modules)
+    module_path_pairs = re.findall(r"\|\s*([^|]+)\s*\|\s*([^|]+)\s*\|", list_modules)
     module_path_pairs = [(module.strip(), path.strip()) for module, path in module_path_pairs]
 
-    #initialize empty dict for storing module times
+    # initialize empty dict for storing module times
     module_times = dict.fromkeys(config_dictionary.keys(), 0)
 
     print("\n   Calculating individual times for your modules... \n")
-    for module in config_dictionary.keys(): #iterate over each module present in the configuration file we are trying to optimize
+    for (
+        module
+    ) in (
+        config_dictionary.keys()
+    ):  # iterate over each module present in the configuration file we are trying to optimize
 
-        #generate the configuration file with single module (no optimization and no propeller)
-        NEW_CONFIGURATION_FILE = pth.join(WORK_FOLDER_PATH, 'config_opti_tmp', str(module))
+        # generate the configuration file with single module (no optimization and no propeller)
+        NEW_CONFIGURATION_FILE = pth.join(WORK_FOLDER_PATH, "config_opti_tmp", str(module))
         if not pth.isdir(NEW_CONFIGURATION_FILE):
             os.makedirs(NEW_CONFIGURATION_FILE)
         shutil.copy(ORIGINAL_CONFIGURATION_FILE, NEW_CONFIGURATION_FILE)
-        FOLDER_WITH_CSVS_SRC = WORK_FOLDER_PATH + '/workdir' 
-        FOLDER_WITH_CSVS_TGT = NEW_CONFIGURATION_FILE + '/workdir'
-        shutil.copytree(FOLDER_WITH_CSVS_SRC, FOLDER_WITH_CSVS_TGT, dirs_exist_ok=True)#copy needed csv files to new config file path
-        NEW_CONFIGURATION_FILE = pth.join(NEW_CONFIGURATION_FILE, os.path.basename(ORIGINAL_CONFIGURATION_FILE))
-        with open(NEW_CONFIGURATION_FILE, 'r') as file:
-                data = yaml.safe_load(file)
-                #Deletes keys inside aircraft sizing except the solvers, if present and the module we want to time
-                keys_to_delete = [key for key in data['model']['aircraft_sizing'] if key not in ['linear_solver', 'nonlinear_solver', module]]
-                for key in keys_to_delete:
-                    del data['model']['aircraft_sizing'][key]
-                #data['model']['aircraft_sizing'] = config_dictionary[module] #this is simpler but eliminates the specified solvers.
-        with open(NEW_CONFIGURATION_FILE, 'w') as file:
-                if 'optimization' in data: del data['optimization'] #no optimization
-                if 'propeller' in data['model']: del data['model']['propeller'] #no propeller
-                yaml.dump(data, file, default_flow_style=False, sort_keys=False)
-    
-        #Fetch the appropriate SOURCE file from unitary_tests of each module, with the paths to the modules listed by the fastoad list_modules ran before
+        FOLDER_WITH_CSVS_SRC = WORK_FOLDER_PATH + "/workdir"
+        FOLDER_WITH_CSVS_TGT = NEW_CONFIGURATION_FILE + "/workdir"
+        shutil.copytree(
+            FOLDER_WITH_CSVS_SRC, FOLDER_WITH_CSVS_TGT, dirs_exist_ok=True
+        )  # copy needed csv files to new config file path
+        NEW_CONFIGURATION_FILE = pth.join(
+            NEW_CONFIGURATION_FILE, os.path.basename(ORIGINAL_CONFIGURATION_FILE)
+        )
+        with open(NEW_CONFIGURATION_FILE, "r") as file:
+            data = yaml.safe_load(file)
+            # Deletes keys inside aircraft sizing except the solvers, if present and the module we want to time
+            keys_to_delete = [
+                key
+                for key in data["model"]["aircraft_sizing"]
+                if key not in ["linear_solver", "nonlinear_solver", module]
+            ]
+            for key in keys_to_delete:
+                del data["model"]["aircraft_sizing"][key]
+            # data['model']['aircraft_sizing'] = config_dictionary[module] #this is simpler but eliminates the specified solvers.
+        with open(NEW_CONFIGURATION_FILE, "w") as file:
+            if "optimization" in data:
+                del data["optimization"]  # no optimization
+            if "propeller" in data["model"]:
+                del data["model"]["propeller"]  # no propeller
+            yaml.dump(data, file, default_flow_style=False, sort_keys=False)
 
-        #Find the id or ids of the module in question
+        # Fetch the appropriate SOURCE file from unitary_tests of each module, with the paths to the modules listed by the fastoad list_modules ran before
+
+        # Find the id or ids of the module in question
         id_of_module = find_id_value(config_dictionary[module])
 
-        #Find the path to its folder
-        path_to_id_of_module = next((pair[1] for pair in module_path_pairs if id_of_module in pair[0]), None)
-        #Find the path to its unitary tests folder, which contains the source file
+        # Find the path to its folder
+        path_to_id_of_module = next(
+            (pair[1] for pair in module_path_pairs if id_of_module in pair[0]), None
+        )
+        # Find the path to its unitary tests folder, which contains the source file
         index = path_to_id_of_module.index("/models")  # Find the index of "/models"
-        next_folder_index = path_to_id_of_module.find("/", index + len("/models") + 1) # Find the index of the folder immediately after models
-             
-        if id_of_module != 'fastga.weight.legacy':
+        next_folder_index = path_to_id_of_module.find(
+            "/", index + len("/models") + 1
+        )  # Find the index of the folder immediately after models
 
-            NEW_SOURCE_FILE = path_to_id_of_module[:next_folder_index] + '/unitary_tests/data/beechcraft_76.xml' #add the path to the source file used for the unitary test of module being timed
-            if id_of_module == 'fastga.loop.mtow':
-                NEW_SOURCE_FILE = path_to_id_of_module[:next_folder_index] + '/mass_breakdown/unitary_tests/data/beechcraft_76.xml' #add the path to the source file used for the unitary test of module being timed
-    
-         ###############################
+        if id_of_module != "fastga.weight.legacy":
+
+            NEW_SOURCE_FILE = (
+                path_to_id_of_module[:next_folder_index] + "/unitary_tests/data/beechcraft_76.xml"
+            )  # add the path to the source file used for the unitary test of module being timed
+            if id_of_module == "fastga.loop.mtow":
+                NEW_SOURCE_FILE = (
+                    path_to_id_of_module[:next_folder_index]
+                    + "/mass_breakdown/unitary_tests/data/beechcraft_76.xml"
+                )  # add the path to the source file used for the unitary test of module being timed
+
+            ###############################
             api_cs25.generate_inputs(NEW_CONFIGURATION_FILE, NEW_SOURCE_FILE, overwrite=True)
 
             executions_time = []
 
-            print('\n   Starting timings of: ', module)
-            for _ in range(15): #run them individually 20 times, to have a good average
+            print("\n   Starting timings of: ", module)
+            for _ in range(15):  # run them individually 20 times, to have a good average
 
                 starting = time.time()
 
-                #Supressing terminal outputs of modules:
+                # Supressing terminal outputs of modules:
 
                 # Save the original standard output and standard error
                 original_stdout = sys.stdout
                 original_stderr = sys.stderr
                 # Redirect standard output and standard error to the null device
-                sys.stdout = open(os.devnull, 'w')
-                sys.stderr = open(os.devnull, 'w')
-                with contextlib.redirect_stdout(None): #supresses outputs by terminal of each small case execution
+                sys.stdout = open(os.devnull, "w")
+                sys.stderr = open(os.devnull, "w")
+                with contextlib.redirect_stdout(
+                    None
+                ):  # supresses outputs by terminal of each small case execution
                     api_cs25.evaluate_problem(NEW_CONFIGURATION_FILE, overwrite=True)
                 # Restore the original standard output and standard error
                 sys.stdout = original_stdout
@@ -97,60 +120,65 @@ def time_modules(config_dictionary, ORIGINAL_CONFIGURATION_FILE, WORK_FOLDER_PAT
 
                 executions_time.append(time.time() - starting)
 
-            module_times[module] = sum(executions_time)/len(executions_time)
-            print('\n       Average time of ', module, ': ', module_times[module], ' seconds\n') 
+            module_times[module] = sum(executions_time) / len(executions_time)
+            print("\n       Average time of ", module, ": ", module_times[module], " seconds\n")
 
+        elif id_of_module == "fastga.weight.legacy":
 
-        elif id_of_module == 'fastga.weight.legacy':
+            CG_SOURCE_FILE = (
+                path_to_id_of_module[:next_folder_index]
+                + "/cg/unitary_tests/data/beechcraft_76.xml"
+            )  # add the path to the source file used for the unitary test of module being timed
 
-            CG_SOURCE_FILE = path_to_id_of_module[:next_folder_index] + '/cg/unitary_tests/data/beechcraft_76.xml' #add the path to the source file used for the unitary test of module being timed
-
-            #we have to include a missing line to the source file in FAST-GA/src/fastga/models/weight/cg, so weight can run on its own
+            # we have to include a missing line to the source file in FAST-GA/src/fastga/models/weight/cg, so weight can run on its own
             tree = ET.parse(CG_SOURCE_FILE)
             root = tree.getroot()
 
             # Find the <geometry> element
-            geometry_element = root.find('data/geometry')
+            geometry_element = root.find("data/geometry")
             if geometry_element is not None:
                 # Create missing element
-                new_element = ET.Element('wing_configuration')
-                new_element.set('is_input', 'True')
-                new_element.text = '1.0'
+                new_element = ET.Element("wing_configuration")
+                new_element.set("is_input", "True")
+                new_element.text = "1.0"
 
                 # Append the new element to the <geometry> element
                 geometry_element.append(new_element)
             else:
-                sys.exit("<geometry> element not found in the XML structure. Could not compute weight times")
+                sys.exit(
+                    "<geometry> element not found in the XML structure. Could not compute weight times"
+                )
 
             # Save the modified XML back to a file
-            WEIGHT_PATH = pth.join(WORK_FOLDER_PATH, 'config_opti_tmp', str(module), 'data')
+            WEIGHT_PATH = pth.join(WORK_FOLDER_PATH, "config_opti_tmp", str(module), "data")
             if not pth.isdir(WEIGHT_PATH):
                 os.makedirs(WEIGHT_PATH)
-            WEIGHT_SOURCE_FILE = pth.join(WEIGHT_PATH, 'beechcraft_76.xml')
+            WEIGHT_SOURCE_FILE = pth.join(WEIGHT_PATH, "beechcraft_76.xml")
             tree.write(WEIGHT_SOURCE_FILE)
 
             api_cs25.generate_inputs(NEW_CONFIGURATION_FILE, WEIGHT_SOURCE_FILE, overwrite=True)
 
             executions_time = []
 
-            print('\n   Starting timings of: ', module)
-            for _ in range(15): #run them individually 20 times, to have a good average
+            print("\n   Starting timings of: ", module)
+            for _ in range(15):  # run them individually 20 times, to have a good average
 
                 starting = time.time()
 
-                with contextlib.redirect_stdout(None): #supresses outputs by terminal of each small case execution
+                with contextlib.redirect_stdout(
+                    None
+                ):  # supresses outputs by terminal of each small case execution
                     api_cs25.evaluate_problem(NEW_CONFIGURATION_FILE, overwrite=True)
 
                 executions_time.append(time.time() - starting)
 
-            module_times[module] = sum(executions_time)/len(executions_time)
+            module_times[module] = sum(executions_time) / len(executions_time)
 
-            print('\n       Average time of ', module, ': ', module_times[module], ' seconds\n')
-         
-        
+            print("\n       Average time of ", module, ": ", module_times[module], " seconds\n")
+
     try:
-        #remove all temporary config files created for unitary timing
-        shutil.rmtree(pth.join(WORK_FOLDER_PATH, 'config_opti_tmp'))
+        # remove all temporary config files created for unitary timing
+        shutil.rmtree(pth.join(WORK_FOLDER_PATH, "config_opti_tmp"))
     except FileNotFoundError:
         pass
-    return  module_times
+    return module_times
