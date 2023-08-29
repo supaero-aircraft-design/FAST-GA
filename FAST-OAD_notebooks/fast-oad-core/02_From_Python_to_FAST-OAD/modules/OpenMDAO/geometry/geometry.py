aac6c121c056486f86b7503007fe7dd28294ad6a
johnathanasd <93048.87051.ko@gmail.com>
2023-08-29 20:34:31 +0800

    clean up

--- FAST-OAD_notebooks/fast-oad-core/02_From_Python_to_FAST-OAD/modules/OpenMDAO/geometry/geometry.py
+++
@@ -1,9 +0,0 @@
-import openmdao.api as om
-
-from .sub_components.compute_wing_area import ComputeWingArea
-
-
-class ComputeGeometry(om.Group):
-    def setup(self):
-
-        self.add_subsystem(name="compute_wing_area", subsys=ComputeWingArea(), promotes=["*"])
--- FAST-OAD_notebooks/fast-oad-core/02_From_Python_to_FAST-OAD/modules/OpenMDAO/performance/performance.py
+++
@@ -1,9 +0,0 @@
-import openmdao.api as om
-
-from .sub_components.compute_fuel_mass import ComputeFuelMass
-
-
-class ComputePerformance(om.Group):
-    def setup(self):
-
-        self.add_subsystem(name="compute_fuel_mass", subsys=ComputeFuelMass(), promotes=["*"])
