922be154a621d34e87d1291a4e96ed8aa7140fb9
johnathanasd <73662010+johnathanasd@users.noreply.github.com>
2025-02-03 08:10:18 +0100

    Correct typo

--- src/fastga/models/weight/mass_breakdown/a_airframe/a32_tail_vtp_weight.py
+++ src/fastga/models/weight/mass_breakdown/a_airframe/a32_tail_vtp_weight.py
@@ -1,5 +1,5 @@
 """
-Python module for htp weight calculation, part of the tail mass computation.
+Python module for vtp weight calculation, part of the tail mass computation.
 """
 #  This file is part of FAST-OAD_CS23 : A framework for rapid Overall Aircraft Design
 #  Copyright (C) 2025  ONERA & ISAE-SUPAERO
