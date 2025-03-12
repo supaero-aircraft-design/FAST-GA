a576c5da209645a001051cd0cc9e4e3bcee4ea5c
johnathanasd <73662010+johnathanasd@users.noreply.github.com>
2025-03-12 14:39:39 +0100

    Remove extra space

--- src/fastga/models/geometry/geom_components/wing/components/compute_wing_sweep_50.py
+++ src/fastga/models/geometry/geom_components/wing/components/compute_wing_sweep_50.py
@@ -1,5 +1,5 @@
 """
-Python module for wing sweep angle calculation at 50% of the MAC , part of the wing sweep angle.
+Python module for wing sweep angle calculation at 50% of the MAC, part of the wing sweep angle.
 """
 #  This file is part of FAST-OAD_CS23 : A framework for rapid Overall Aircraft Design
 #  Copyright (C) 2025 ONERA & ISAE-SUPAERO
