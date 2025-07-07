7f433c21ef831618e5fd0f3bb5d7cf0506844878
johnathanasd <73662010+johnathanasd@users.noreply.github.com>
2025-07-07 17:42:38 +0200

    Add new function for later separation, k_fus not finished

---
+++ src/fastga/models/aerodynamics/components/digitization/compute_k_fuselage.py
@@ -0,0 +1,98 @@
+#  This file is part of FAST-OAD_CS23 : A framework for rapid Overall Airchord_ratioaft Design
+#  Copyright (C) 2025  ONERA & ISAE-SUPAERO
+#  FAST is free software: you can redistribute it and/or modify
+#  it under the terms of the GNU General Public License as published by
+#  the Free Software Foundation, either version 3 of the License, or
+#  (at your option) any later version.
+#  This program is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU General Public License for more details.
+#  You should have received a copy of the GNU General Public License
+#  along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+import numpy as np
+import openmdao.api as om
+import logging
+
+_LOGGER = logging.getLogger(__name__)
+
+
+class ComputeFuselagePitchMomentFactor(om.ExplicitComponent):
+    """
+    Raymer data to estimate the empirical pitching moment factor K_fus (figure 16.14), from :cite:'raymer:2012'.
+
+    :param root_quarter_chord_position_ratio: the position of the root quarter chord of the
+    wing from the nose.
+    divided by the total length of the fuselage.
+    :return k_fus: the empirical pitching moment factor.
+    """
+
+    # pylint: disable=missing-function-docstring
+    # Overriding OpenMDAO setup
+    def setup(self):
+        self.add_input("flap_angle", val=0.0, units="deg")
+        self.add_input("chord_ratio", val=np.nan)
+
+        self.add_output("lift_effectiveness", val=0.1)
+
+        self.declare_partials(of="lift_effectiveness", wrt="*", method="exact")
+
+    # pylint: disable=missing-function-docstring, unused-argument
+    # Overriding OpenMDAO compute, not all arguments are used
+    def compute(self, inputs, outputs, discrete_inputs=None, discrete_outputs=None):
+        flap_angle = inputs["flap_angle"]
+        chord_ratio = inputs["chord_ratio"]
+
+        if flap_angle != np.clip(flap_angle, -0.08, 79.32):
+            flap_angle = np.clip(flap_angle, -0.08, 79.32)
+            _LOGGER.warning("Flap angle value outside of the range in Roskam's book, value clipped")
+
+        if chord_ratio != np.clip(chord_ratio, 0.15, 0.4):
+            chord_ratio = np.clip(chord_ratio, 0.15, 0.4)
+            _LOGGER.warning(
+                "Chord ratio value outside of the range in Roskam's book, value clipped"
+            )
+
+        outputs["lift_effectiveness"] = (
+            0.0239
+            + 0.006 * flap_angle
+            + 2.6633 * chord_ratio
+            - 0.0002 * flap_angle**2.0
+            - 0.0121 * flap_angle * chord_ratio
+            - 2.9929 * chord_ratio**2.0
+            - 0.0002 * flap_angle**2.0 * chord_ratio
+            + 0.0354 * flap_angle * chord_ratio**2.0
+            - 0.5931 * chord_ratio**3.0
+        )
+
+    # pylint: disable=missing-function-docstring, unused-argument
+    # Overriding OpenMDAO compute_partials, not all arguments are used
+    def compute_partials(self, inputs, partials, discrete_inputs=None):
+        flap_angle = inputs["flap_angle"]
+        chord_ratio = inputs["chord_ratio"]
+
+        partials["lift_effectiveness", "flap_angle"] = np.where(
+            flap_angle == np.clip(flap_angle, -0.08, 79.32),
+            (
+                0.006
+                - 0.0004 * flap_angle
+                - 0.0121 * chord_ratio
+                - 0.0004 * flap_angle * chord_ratio
+                + 0.0354 * chord_ratio**2.0
+            ),
+            1e-6,
+        )
+
+        partials["lift_effectiveness", "chord_ratio"] = np.where(
+            chord_ratio == np.clip(chord_ratio, 0.15, 0.4),
+            (
+                2.6633
+                - 0.0121 * flap_angle
+                - 5.9858 * chord_ratio
+                - 0.0002 * flap_angle**2.0
+                + 0.0708 * flap_angle * chord_ratio
+                - 1.7793 * chord_ratio**2.0
+            ),
+            1e-6,
+        )
--- src/fastga/models/aerodynamics/components/fuselage/compute_cm_alpha_fus.py
+++ src/fastga/models/aerodynamics/components/fuselage/compute_cm_alpha_fus.py
@@ -1,6 +1,6 @@
 """Estimation of fuselage pitching moment."""
 #  This file is part of FAST-OAD_CS23 : A framework for rapid Overall Aircraft Design
-#  Copyright (C) 2022  ONERA & ISAE-SUPAERO
+#  Copyright (C) 2025  ONERA & ISAE-SUPAERO
 #  FAST is free software: you can redistribute it and/or modify
 #  it under the terms of the GNU General Public License as published by
 #  the Free Software Foundation, either version 3 of the License, or
@@ -13,6 +13,7 @@
 #  along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
 import numpy as np
+import openmdao.api as om
 import fastoad.api as oad
 
 from fastga.models.aerodynamics.components.figure_digitization import FigureDigitization
@@ -61,3 +62,97 @@ class ComputeCmAlphaFuselage(FigureDigitization):
         cm_alpha_fus = -k_fus * width_max**2 * fus_length / (l0_wing * wing_area) * 180.0 / np.pi
 
         outputs["data:aerodynamics:fuselage:cm_alpha"] = cm_alpha_fus
+
+
+class ComputeQuarterRootChordPositionRatio(om.ExplicitComponent):
+    """
+    Computation of the position of the quarter wing root chord from the nose divided by the total
+    fuselage length.
+    """
+
+    def setup(self):
+        self.add_input("data:geometry:wing:MAC:leading_edge:x:local", val=np.nan, units="m")
+        self.add_input("data:geometry:wing:MAC:length", val=np.nan, units="m")
+        self.add_input("data:geometry:wing:MAC:at25percent:x", val=np.nan, units="m")
+        self.add_input("data:geometry:wing:root:virtual_chord", val=np.nan, units="m")
+        self.add_input("data:geometry:fuselage:length", val=np.nan, units="m")
+
+        self.add_output("x0_ratio", val=0.02)
+
+        self.declare_partials("x0_ratio", "*", method="exact")
+
+    def compute(self, inputs, outputs, discrete_inputs=None, discrete_outputs=None):
+        x_wing = inputs["data:geometry:wing:MAC:at25percent:x"]
+        x0_wing = inputs["data:geometry:wing:MAC:leading_edge:x:local"]
+        l0_wing = inputs["data:geometry:wing:MAC:length"]
+        l1_wing = inputs["data:geometry:wing:root:virtual_chord"]
+        fus_length = inputs["data:geometry:fuselage:length"]
+
+        outputs["x0_ratio"] = (x_wing - 0.25 * l0_wing - x0_wing + 0.25 * l1_wing) / fus_length
+
+    def compute_partials(self, inputs, partials, discrete_inputs=None):
+        x_wing = inputs["data:geometry:wing:MAC:at25percent:x"]
+        x0_wing = inputs["data:geometry:wing:MAC:leading_edge:x:local"]
+        l0_wing = inputs["data:geometry:wing:MAC:length"]
+        l1_wing = inputs["data:geometry:wing:root:virtual_chord"]
+        fus_length = inputs["data:geometry:fuselage:length"]
+
+        partials["x0_ratio", "data:geometry:fuselage:length"] = -(
+            (x_wing - 0.25 * l0_wing - x0_wing + 0.25 * l1_wing) / fus_length**2.0
+        )
+        partials["x0_ratio", "data:geometry:wing:MAC:at25percent:x"] = 1.0 / fus_length
+        partials["x0_ratio", "data:geometry:wing:MAC:length"] = -0.25 / fus_length
+        partials["x0_ratio", "data:geometry:wing:MAC:leading_edge:x:local"] = -1.0 / fus_length
+        partials["x0_ratio", "data:geometry:wing:root:virtual_chord"] = 0.25 / fus_length
+
+
+class _ComputeCmAlphaFuselage(om.ExplicitComponent):
+    """
+    Estimation of the fuselage pitching moment using the methodology described in section 16.3.8
+    of Raymer :cite:`raymer:2012`.
+    """
+
+    def setup(self):
+        self.add_input("data:geometry:wing:MAC:length", val=np.nan, units="m")
+        self.add_input("data:geometry:wing:area", val=np.nan, units="m**2")
+        self.add_input("data:geometry:fuselage:length", val=np.nan, units="m")
+        self.add_input("data:geometry:fuselage:maximum_width", val=np.nan, units="m")
+        self.add_input("fuselage_pitch_moment_factor", val=np.nan)
+
+        self.add_output("data:aerodynamics:fuselage:cm_alpha", units="deg**-1")
+
+        self.declare_partials("data:aerodynamics:fuselage:cm_alpha", "*", method="exact")
+
+    def compute(self, inputs, outputs, discrete_inputs=None, discrete_outputs=None):
+        wing_area = inputs["data:geometry:wing:area"]
+        l0_wing = inputs["data:geometry:wing:MAC:length"]
+        fus_length = inputs["data:geometry:fuselage:length"]
+        width_max = inputs["data:geometry:fuselage:maximum_width"]
+        k_fus = inputs["fuselage_pitch_moment_factor"]
+
+        outputs["data:aerodynamics:fuselage:cm_alpha"] = (
+            -k_fus * width_max**2.0 * fus_length / (l0_wing * wing_area)
+        )
+
+    def compute_partials(self, inputs, partials, discrete_inputs=None):
+        wing_area = inputs["data:geometry:wing:area"]
+        l0_wing = inputs["data:geometry:wing:MAC:length"]
+        fus_length = inputs["data:geometry:fuselage:length"]
+        width_max = inputs["data:geometry:fuselage:maximum_width"]
+        k_fus = inputs["fuselage_pitch_moment_factor"]
+
+        partials["data:aerodynamics:fuselage:cm_alpha", "data:geometry:wing:area"] = (
+            k_fus * width_max**2.0 * fus_length / (l0_wing * wing_area**2.0)
+        )
+        partials["data:aerodynamics:fuselage:cm_alpha", "data:geometry:wing:MAC:length"] = (
+            k_fus * width_max**2.0 * fus_length / (l0_wing**2.0 * wing_area)
+        )
+        partials["data:aerodynamics:fuselage:cm_alpha", "data:geometry:fuselage:length"] = (
+            -k_fus * width_max**2.0 / (l0_wing * wing_area)
+        )
+        partials["data:aerodynamics:fuselage:cm_alpha", "data:geometry:fuselage:maximum_width"] = (
+            -2.0 * k_fus * width_max * fus_length / (l0_wing * wing_area)
+        )
+        partials["data:aerodynamics:fuselage:cm_alpha", "fuselage_pitch_moment_factor"] = (
+            -(width_max**2.0) * fus_length / (l0_wing * wing_area)
+        )
