c0a6c699d3ab358eb04735d4979e18f1eee46670
johnathanasd <73662010+johnathanasd@users.noreply.github.com>
2025-07-09 10:08:54 +0200

    Resolve codacy issue

--- src/fastga/models/aerodynamics/components/methodology/k_fus_regression.py
+++ src/fastga/models/aerodynamics/components/methodology/k_fus_regression.py
@@ -1,3 +1,6 @@
+"""
+Python module to obtain empirical pitching moment factor coefficient regression model.
+"""
 #  This file is part of FAST-OAD_CS23 : A framework for rapid Overall Aircraft Design
 #  Copyright (C) 2025  ONERA & ISAE-SUPAERO
 #  FAST is free software: you can redistribute it and/or modify
