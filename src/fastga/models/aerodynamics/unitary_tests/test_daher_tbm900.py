2ce5dcb7cdf32ee1b8cb86709e589f4cfe6e6a3c
johnathanasd <73662010+johnathanasd@users.noreply.github.com>
2025-08-18 08:45:17 +0200

    Resolve error issue

--- src/fastga/models/aerodynamics/unitary_tests/test_daher_tbm900.py
+++ src/fastga/models/aerodynamics/unitary_tests/test_daher_tbm900.py
@@ -489,7 +489,7 @@ def test_vlm_comp_low_speed():
     )
 
 
-@pytest.mark.skipif(system() != "Windows", reason="May result in unexpected values")
+@pytest.mark.skipif(reason="May result in unexpected results")
 def test_vlm_comp_low_speed_neuralfoil():
     """Tests vlm components @ low speed."""
     y_vector_wing = np.array(
